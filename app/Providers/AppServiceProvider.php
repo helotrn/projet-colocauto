d5a4233ee775ee1777bab1adbda09d92b54dd098
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2019-12-02 11:53:27 -0500

    Install and enable passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,38 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
+    public function register() {
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+    public function boot() {
+        Client::creating(function (Client $client) {
+            $client->incrementing = false;
+            $client->id = $this->generateClientId();
+        });
+
+        Client::retrieved(function (Client $client) {
+            $client->incrementing = false;
+        });
+    }
+
+    private function generateClientId(
+        int $length = 16,
+        string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ): string {
+        if ($length < 1) {
+            throw new \RangeException('Length must be a positive integer');
+        }
+
+        $pieces = [];
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $pieces []= $keyspace[random_int(0, $max)];
+        }
+
+        return implode('', $pieces);
     }
 }
