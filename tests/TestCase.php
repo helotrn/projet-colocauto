602110ca30267bb021f04a5a66356008b2cb6bf4
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2019-12-09 20:42:12 -0500

    Act as certain user in all test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,16 +2,25 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Faker\Factory;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Passport\Passport;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseMigrations;
+
     protected $faker;
-    public function setUp() {
+    protected $user;
+
+    public function setUp(): void {
         parent::setUp();
+
+        $this->user = factory(User::class)->create();
+
+        Passport::actingAs($this->user);
         $this->faker = Factory::create();
     }
 }
