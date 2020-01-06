d1ae031fcb93cd232647e1cbdb8f9943194c5a8a
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-01-06 13:37:24 -0500

    Prepare VUE application

---
+++ resources/app/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="locomotion">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
---
+++ resources/app/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import '@/assets/scss/main.scss';
+
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
