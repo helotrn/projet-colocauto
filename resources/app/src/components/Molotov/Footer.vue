af1c3ad7b5e225213d6179ef04a43b0c47189a06
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-01-06 14:40:34 -0500

    Implement Home page structure

---
+++ resources/app/src/components/Molotov/Footer.vue
@@ -0,0 +1,106 @@
+<template>
+  <b-container fluid class="molotov-footer">
+    <b-row>
+      <b-col>
+        <div class="molotov-footer__inner">
+          <a href="https://molotov.ca" target="_blank">
+            <span>Créé avec</span>
+            <span class="love"><svg-coeur /></span>
+            <span>par Molotov</span>
+          </a>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import Coeur from '@/assets/svg/coeur.svg';
+
+export default {
+  name: 'MolotovFooter',
+  components: {
+    'svg-coeur': Coeur,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap-grid";
+
+$molotov-footer-color: #fff;
+$molotov-footer-hover-color: #fff;
+$molotov-footer-background-color: #000;
+$molotov-footer-heart-size: 20px !default;
+$molotov-footer-heart-fill: $molotov-footer-background-color !default;
+$molotov-footer-heart-stroke: #fc4e4e !default;
+$molotov-footer-heart-stroke-width: 2px !default;
+$molotov-footer-heart-fill-hover: $molotov-footer-heart-fill !default;
+
+@keyframes heartbeat {
+  0% {
+    transform: scale(1);
+  }
+
+  25% {
+    transform: scale(1.1);
+  }
+
+  40% {
+    transform: scale(1);
+  }
+
+  60% {
+    transform: scale(1.1);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+
+.molotov-footer {
+  background-color: $molotov-footer-background-color;
+
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+
+  &__inner {
+    margin-top: 12px;
+    margin-bottom: 12px;
+
+    a {
+      color: $molotov-footer-color;
+      fill: $molotov-footer-heart-fill;
+      stroke: $molotov-footer-heart-stroke;
+      transition: color $one-tick ease-in-out;
+      text-decoration: none;
+
+      .love {
+        display: inline-block;
+        transition: fill $one-tick ease-in-out;
+        width: $molotov-footer-heart-size + 2 * $molotov-footer-heart-stroke-width * 2;
+      }
+
+      &:hover {
+        text-decoration: none;
+        color: $molotov-footer-hover-color;
+
+        .love {
+          fill: $molotov-footer-heart-fill-hover;
+          animation: 0.8s infinite heartbeat;
+        }
+      }
+    }
+
+    svg {
+      overflow: visible;
+      display: inline;
+      height: $molotov-footer-heart-size;
+      width: $molotov-footer-heart-size;
+      stroke-width: $molotov-footer-heart-stroke-width;
+    }
+  }
+}
+</style>
--- resources/app/src/views/Home.vue
+++ resources/app/src/views/Home.vue
@@ -1,11 +1,46 @@
 <template>
   <div class="home page">
-    <h1>Locomotion</h1>
+    <header>
+      <h1>Locomotion</h1>
+    </header>
+    <b-container tag="main" class="page__content">
+      <b-row tag="section">
+        <b-col>
+          <h2 class="page__section__title">
+            La page d'accueil
+          </h2>
+          <div class="page__section__content">
+            <p>Un texte personnalisé. Ici, la page d'accueil.</p>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row tag="section">
+        <b-col>
+          <h2 class="page__section__title">
+            Se connecter
+          </h2>
+          <div class="page__section__content">
+            <p>Je suis convaincu, je veux essayer.</p>
+
+            <b-btn variant="primary" to="login">Se connecter</b-btn>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+    <footer class="page__footer">
+      <molotov-footer />
+    </footer>
   </div>
 </template>
 
 <script>
+import MolotovFooter from '@/components/Molotov/Footer.vue';
+
 export default {
   name: 'Home',
+  components: { MolotovFooter },
 };
 </script>
+
+<style lang="scss">
+</style>
