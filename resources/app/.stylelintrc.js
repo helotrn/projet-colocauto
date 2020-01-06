afb08b94d04187afb681b8a46709e35f1f41d3c2
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-01-06 18:37:49 -0500

    Disable a stylelint check

--- resources/app/.stylelintrc.js
+++ resources/app/.stylelintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
   root: true,
-  'extends': 'stylelint-config-primer',
+  extends: 'stylelint-config-primer',
   rules: {
     'at-rule-blacklist': [],
     'order/properties-order': null,
     'selector-max-type': null,
     'max-nesting-depth': 4,
     'selector-list-comma-newline-after': 'always-multi-line',
+    'plugin/no-unsupported-browser-features': ['warn', {
+      'browsers': ['> 5%'],
+      'ignore': ['rem', 'animation', 'css-transition']
+    }]
   },
 };
