bc34ee0f03f6eac0afb579da15674c63f3f037b1
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-01-06 14:40:27 -0500

    Add vue-svg-loader for inline svg

--- resources/app/src/App.vue
+++ resources/app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="locomotion">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
--- resources/app/vue.config.js
+++ resources/app/vue.config.js
@@ -1,6 +1,18 @@
 const path = require('path');
 
 module.exports = {
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
   css: {
     loaderOptions: {
       sass: {
