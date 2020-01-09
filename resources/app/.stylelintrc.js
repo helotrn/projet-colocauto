25bac60a9ba8a86202566b018e309309977db917
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-01-09 15:09:34 -0500

    Add dropdown section in layout-navbar

--- resources/app/.stylelintrc.js
+++ resources/app/.stylelintrc.js
@@ -10,6 +10,11 @@ module.exports = {
     'plugin/no-unsupported-browser-features': ['warn', {
       'browsers': ['> 5%'],
       'ignore': ['rem', 'animation', 'css-transition']
-    }]
+    }],
+    'selector-no-qualifying-type': [true, {
+      ignore: ["attribute", "class"],
+    }],
+    'selector-max-compound-selectors': 4,
+    'selector-max-specificity': '0,5,0',
   },
 };
--- resources/app/src/components/Layout/Navbar.vue
+++ resources/app/src/components/Layout/Navbar.vue
@@ -5,7 +5,7 @@
         <img class="layout-navbar__logo" src="/logo.png">
       </b-navbar-brand>
       <b-navbar-brand class="layout-navbar__separator" v-if="pageTitle"/>
-        <b-navbar-brand>{{ pageTitle }}</b-navbar-brand>
+      <b-navbar-brand>{{ pageTitle }}</b-navbar-brand>
     </div>
 
     <b-navbar-toggle target="nav-collapse" />
@@ -16,6 +16,18 @@
         <b-nav-item href="/map" v-if="hasCommunity">Trouver un véhicule</b-nav-item>
         <b-nav-item to="/community" v-if="hasCommunity">Communauté</b-nav-item>
         <b-nav-item href="/map" v-if="!hasCommunity">Trouver une communauté</b-nav-item>
+
+        <b-nav-item-dropdown class="layout-navbar__dropdown" text="" right>
+          <template v-slot:button-content>
+            <b-badge pill variant="locomotion" class="layout-navbar__dropdown__icon">
+              <svg-profile />
+            </b-badge>
+          </template>
+          <b-dropdown-item href="#">Compte</b-dropdown-item>
+          <b-dropdown-item href="#">Aide</b-dropdown-item>
+          <b-dropdown-divider />
+          <b-dropdown-item href="#">Déconnexion</b-dropdown-item>
+        </b-nav-item-dropdown>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto" v-else>
         <b-nav-item to="/login">Se connecter</b-nav-item>
@@ -27,9 +39,14 @@
 <script>
 import Authenticated from '@/mixins/Authenticated';
 
+import Profile from '@/assets/svg/profile.svg';
+
 export default {
   name: 'Navbar',
   mixins: [Authenticated],
+  components: {
+    'svg-profile': Profile,
+  },
   computed: {
     hasCommunity() {
       return this.loggedIn;
@@ -60,6 +77,24 @@ export default {
     width: 1px;
     height: 36px;
     background-color: $locomotion-green;
+  }
+
+  &.navbar-light .navbar-nav {
+    .layout-navbar__dropdown {
+      margin-left: 60px;
+
+      .nav-link {
+        padding: 0;
+        padding-right: 0;
+        padding-left: 0;
+      }
+
+      &__icon {
+        width: $line-height-base + (2 * $nav-link-padding-y);
+        height: $line-height-base + (2 * $nav-link-padding-y);
+        padding: 9px;
+      }
+    }
   }
 }
 </style>
