3b412f3c036ab583c51012d8f27a98192929a7d4
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-01-09 17:43:14 -0500

    Add mutations and actions template to RestModule

--- resources/app/src/store/RestModule.js
+++ resources/app/src/store/RestModule.js
@@ -1,20 +1,44 @@
-export default function RestModule(slug, state) {
+export default function RestModule(slug, initialState) {
   return {
+    namespaced: true,
     state: {
-      ...state,
+      ...initialState,
       ajax: null,
       data: [],
       dataById: {},
       loaded: false,
-      loading: true,
+      loading: false,
       search: [],
       slug,
       total: undefined,
     },
     mutations: {
-
+      ajax(state, ajax) {
+        state.ajax = ajax;
+      },
+      data(state, data) {
+        state.data.splice(0, data.length, data);
+      },
+      addData(state, data) {
+        state.data.push(...data);
+      },
+      loaded(state, loaded) {
+        state.loaded = loaded;
+      },
+      loading(state, loading) {
+        state.loading = loading;
+      },
+      setSearch(state, search) {
+        state.search.splice(0, state.search, search);
+      },
+      setTotal(state, total) {
+        state.total = total;
+      }
     },
     actions: {
+      load({ state, commit }) {
+        console.log('load');
+      },
     },
   };
 }
