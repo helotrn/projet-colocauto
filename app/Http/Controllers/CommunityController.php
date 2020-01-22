e91282222340f225eb556a7a5136045c0e0d6091
Alexandre Chouinard <achouinard31@gmail.com>
2020-01-22 13:50:43 -0500

    add community form requests

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BaseRequest as Request;
 use App\Http\Requests\Community\CreateRequest;
+use App\Http\Requests\Community\DestroyRequest;
+use App\Http\Requests\Community\IndexRequest;
+use App\Http\Requests\Community\RetrieveRequest;
+use App\Http\Requests\Community\UpdateRequest;
 use App\Models\Community;
 use App\Repositories\CommunityRepository;
 
@@ -14,7 +18,7 @@ class CommunityController extends RestController
         $this->model = $model;
     }
 
-    public function index(Request $request) {
+    public function index(IndexRequest $request) {
         try {
             [$items, $total] = $this->repo->get($request);
         } catch (ValidationException $e) {
@@ -27,6 +31,33 @@ class CommunityController extends RestController
     public function create(CreateRequest $request) {
         try {
             $response = parent::validateAndCreate($request);
+        } catch (ValidationException $e) {
+            return $this->respondWithErrors($e->getErrors(), $e->getMessage());
+        }
+        return $response;
+    }
+
+    public function update(UpdateRequest $request, $id) {
+        try {
+            $response = parent::validateAndUpdate($request, $id);
+        } catch (ValidationException $e) {
+            return $this->respondWithErrors($e->getErrors(), $e->getMessage());
+        }
+        return $response;
+    }
+
+    public function retrieve(RetrieveRequest $request, $id) {
+        try {
+            $response = $this->respondWithItem($request, $id);
+        } catch (ValidationException $e) {
+            return $this->respondWithErrors($e->getErrors(), $e->getMessage());
+        }
+        return $response;
+    }
+
+    public function destroy(DestroyRequest $request, $id) {
+        try {
+            $response = parent::validateAndDestroy($request, $id);
         } catch (ValidationException $e) {
             return $this->respondWithErrors($e->getErrors(), $e->getMessage());
         }
---
+++ app/Http/Requests/Community/DestroyRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\Community;
+
+use App\Http\Requests\BaseRequest;
+
+class DestroyRequest extends BaseRequest
+{
+    public function authorize() {
+        return true;
+    }
+
+    public function rules() {
+        $rules = [
+        ];
+
+        return $rules;
+    }
+
+    public function messages() {
+        return [
+        ];
+    }
+}
