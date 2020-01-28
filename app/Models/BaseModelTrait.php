5704efc7f2e647ae8f9ba759736d2423d16351b2
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-01-28 17:34:13 -0500

    Fix incorrect use in BaseModel

--- app/Models/BaseModelTrait.php
+++ app/Models/BaseModelTrait.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Transformers\BaseTransformer;
+use Illuminate\Database\Eloquent\Builder;
+
 trait BaseModelTrait
 {
     public static $transformer = BaseTransformer::class;
