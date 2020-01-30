84e1755db44b19b7784798f132c27d8f314d4017
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-01-30 11:26:00 -0500

    Export admin routes in js router

---
+++ resources/app/src/router/admin.js
@@ -0,0 +1,87 @@
+import Admin from '../views/Admin.vue';
+import AdminDashboard from '../views/admin/Dashboard.vue';
+import AdminCommunities from '../views/admin/Communities.vue';
+import AdminCommunity from '../views/admin/Community.vue';
+
+export default {
+  path: '/admin',
+  component: Admin,
+  meta: {
+    auth: true,
+  },
+  children: [
+    {
+      path: '',
+      component: AdminDashboard,
+      meta: {
+        auth: true,
+      },
+    },
+    {
+      path: 'communities',
+      component: AdminCommunities,
+      meta: {
+        auth: true,
+        creatable: true,
+        slug: 'communities',
+        data: {
+          communities: {
+            retrieve: {
+              fields: 'id,name',
+            },
+          },
+        },
+        title: 'Communautés',
+      },
+    },
+    {
+      path: 'communities/:id',
+      component: AdminCommunity,
+      props: true,
+      meta: {
+        auth: true,
+        slug: 'communities',
+        params: {
+          fields: '*,users.*',
+        },
+        form: {
+          id: {
+            type: 'number',
+            disabled: true,
+            required: true,
+            label: 'ID',
+          },
+          name: {
+            type: 'text',
+            required: true,
+            label: 'Nom',
+          },
+          description: {
+            type: 'textarea',
+            required: true,
+            label: 'Description',
+          },
+          type: {
+            type: 'select',
+            label: 'Type',
+            options: [
+              {
+                text: 'Privée',
+                value: 'private',
+              },
+              {
+                text: 'Voisinage',
+                value: 'neighborhood',
+              },
+              {
+                text: 'Quartier',
+                value: 'borough',
+              },
+            ],
+          },
+        },
+      },
+    },
+  ],
+};
+
--- resources/app/src/router/index.js
+++ resources/app/src/router/index.js
@@ -9,13 +9,9 @@ import Login from '../views/Login.vue';
 import CommunityMap from '../views/community/Map.vue';
 
 import Register from '../views/Register.vue';
-import Admin from '../views/Admin.vue';
-import AdminDashboard from '../views/admin/Dashboard.vue';
-import AdminCommunities from '../views/admin/Communities.vue';
-import AdminCommunity from '../views/admin/Community.vue';
-
-
 import RegistrationMap from '../views/registration/Map.vue';
+
+import adminRoutes from './admin';
 
 Vue.use(VueRouter);
 
@@ -95,87 +91,7 @@ const routes = [
       title: 'Aide de Locomotion',
     },
   },
-  {
-    path: '/admin',
-    component: Admin,
-    meta: {
-      auth: true,
-    },
-    children: [
-      {
-        path: '',
-        component: AdminDashboard,
-        meta: {
-          auth: true,
-        },
-      },
-      {
-        path: 'communities',
-        component: AdminCommunities,
-        meta: {
-          auth: true,
-          creatable: true,
-          slug: 'communities',
-          data: {
-            communities: {
-              retrieve: {
-                fields: 'id,name',
-              },
-            },
-          },
-          title: 'Communautés',
-        },
-      },
-      {
-        path: 'communities/:id',
-        component: AdminCommunity,
-        props: true,
-        meta: {
-          auth: true,
-          slug: 'communities',
-          params: {
-            fields: '*,users.*',
-          },
-          form: {
-            id: {
-              type: 'number',
-              disabled: true,
-              required: true,
-              label: 'ID',
-            },
-            name: {
-              type: 'text',
-              required: true,
-              label: 'Nom',
-            },
-            description: {
-              type: 'textarea',
-              required: true,
-              label: 'Description',
-            },
-            type: {
-              type: 'select',
-              label: 'Type',
-              options: [
-                {
-                  text: 'Privée',
-                  value: 'private',
-                },
-                {
-                  text: 'Voisinage',
-                  value: 'neighborhood',
-                },
-                {
-                  text: 'Quartier',
-                  value: 'borough',
-                },
-              ],
-            },
-          },
-        },
-      },
-    ],
-  },
+  adminRoutes,
 ];
 
 const router = new VueRouter({
