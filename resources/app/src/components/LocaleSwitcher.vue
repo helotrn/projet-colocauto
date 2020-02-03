607681e88f239cddf77fdea1921cc2291a582fab
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-02-03 15:08:20 -0500

    Add locale-switcher component

---
+++ resources/app/src/components/LocaleSwitcher.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-nav-item-dropdown class="locale-switcher" :text="currentLocale" right>
+    <b-dropdown-item @click="currentLocale = locale"
+      v-for="locale in otherLocales" :key="locale">
+      {{ $t(`locales.${locale}`) }}
+    </b-dropdown-item>
+  </b-nav-item-dropdown>
+</template>
+
+<script>
+export default {
+  name: 'LocaleSwitcher',
+  data() {
+    return {
+      locales: ['en', 'fr'],
+    };
+  },
+  computed: {
+    currentLocale: {
+      get() {
+        return this.$i18n.locale;
+      },
+      set(val) {
+        this.$i18n.locale = val;
+      },
+    },
+    otherLocales() {
+      return this.locales.filter(l => l !== this.currentLocale);
+    },
+  },
+};
+</script>
+
+<style>
+.locale-switcher .dropdown-toggle {
+  text-transform: uppercase;
+}
+</style>
