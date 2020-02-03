7fe482085a5440292df8934c85a9e7116904b222
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-02-03 17:26:04 -0500

    Don't implicitly add all communities to admin user

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -10,17 +10,6 @@ class UserTransformer extends BaseTransformer
     public function transform($item, $options = []) {
         $output = parent::transform($item, $options);
 
-        if (Auth::user()->role === 'admin') {
-            if ($this->shouldIncludeField('communities', $options)) {
-                $output['communities'] = $this->addCollection(
-                    'communities',
-                    Community::all(),
-                    new Community::$transformer(),
-                    $options
-                );
-            }
-        }
-
         if (isset($options['context']['Community']) && $options['pivot']) {
             $output['role'] = $options['pivot']['role'];
         }
