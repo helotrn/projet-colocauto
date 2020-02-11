5bcf1c7d57d1399017b1ee275143583f8f877057
Alexandre Chouinard <achouinard31@gmail.com>
2020-02-11 10:56:56 -0500

    add fillables and approval in borrower

--- app/Models/Borrower.php
+++ app/Models/Borrower.php
@@ -13,12 +13,16 @@ class Borrower extends BaseModel
         'drivers_license_number' => 'nullable',
         'has_been_sued_last_ten_years' => 'boolean',
         'noke_id' => 'nullable',
+        'approved_at' => 'nullable|date',
+        'submitted_at' => 'nullable|date',
     ];
 
     protected $fillable = [
         'drivers_license_number',
         'has_been_sued_last_ten_years',
         'noke_id',
+        'approved_at',
+        'submitted_at',
     ];
 
     public static $transformer = BorrowerTransformer::class;
@@ -33,5 +37,15 @@ class Borrower extends BaseModel
 
     public function loans() {
         return $this->hasMany(Loan::class);
+    }
+
+    public function isApproved() {
+        return $this->approved_at !== null;
+    }
+
+    public function approve($user) {
+        if ($user->isAdmin()) {
+            $this->approved_at = now();
+        }
     }
 }
