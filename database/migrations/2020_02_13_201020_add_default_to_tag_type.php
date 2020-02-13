7800ed6d1295aeaaea04787b067546231b08489c
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-02-13 15:56:55 -0500

    Add default value to tags type

---
+++ database/migrations/2020_02_13_201020_add_default_to_tag_type.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
+
+class AddDefaultToTagType extends Migration
+{
+    public function __construct() {
+        Type::addType('enum', StringType::class);
+    }
+
+    public function up() {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->enum('type', ['tag'])->default('tag')->change();
+        });
+    }
+
+    public function down() {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->enum('type', ['tag'])->default(null)->change();
+        });
+    }
+}
