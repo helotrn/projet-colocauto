a7f71c3094f2ca16a771403206d7442a51b05a9e
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-02-14 15:18:14 -0500

    Introduce respondWithMessage error response type

--- app/Utils/Traits/ErrorResponseTrait.php
+++ app/Utils/Traits/ErrorResponseTrait.php
@@ -4,9 +4,20 @@ namespace App\Utils\Traits;
 
 trait ErrorResponseTrait
 {
+    protected static $errorMessages = [
+        422 => 'Validation errors',
+        400 => 'Bad request',
+    ];
+
+    protected function respondWithMessage($message = null, $status = 400) {
+        return response()->json([
+            'message' => $message ?: dig(static::$errorMessages, $status, 'Error'),
+        ], $status);
+    }
+
     protected function respondWithErrors($errors, $message = null) {
         return response()->json([
-            'message' => $message ?: 'Validation error',
+            'message' => $message ?: static::$errorMessages[422],
             'errors' => $errors,
         ], 422);
     }
