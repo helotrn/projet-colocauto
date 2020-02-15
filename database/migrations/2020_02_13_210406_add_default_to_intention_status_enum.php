6f865e1de1db90f942abad82008c33c1bfee2932
Alexandre Chouinard <achouinard31@gmail.com>
2020-02-15 12:08:46 -0500

    Add migration addDefaultToIntentionStatusEnum

---
+++ database/migrations/2020_02_13_210406_add_default_to_intention_status_enum.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
+
+class AddDefaultToIntentionStatusEnum extends Migration
+{
+    public function __construct() {
+        Type::addType('enum', StringType::class);
+    }
+
+    public function up() {
+        Schema::table('intentions', function (Blueprint $table) {
+            $table->enum('status', ['in_process', 'canceled', 'completed'])->default('in_process')->change();
+        });
+    }
+
+    public function down() {
+        Schema::table('intentions', function (Blueprint $table) {
+            $table->enum('status', ['in_process', 'canceled', 'completed'])->default(null)->change();
+        });
+    }
+}
