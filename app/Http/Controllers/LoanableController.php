01652fb61dc44f59e01e2a5ff2cc2ee930193241
Alexandre Chouinard <achouinard31@gmail.com>
2020-02-19 14:14:02 -0500

    Add car and trailer cases in LoanableController

--- app/Http/Controllers/LoanableController.php
+++ app/Http/Controllers/LoanableController.php
@@ -11,11 +11,15 @@ use Validator;
 class LoanableController extends RestController
 {
     protected $bikeController;
+    protected $carController;
+    protected $trailerController;
 
-    public function __construct(LoanableRepository $repository, Loanable $model, BikeController $bikeController) {
+    public function __construct(LoanableRepository $repository, Loanable $model, BikeController $bikeController, CarController $carController, TrailerController $trailerController) {
         $this->repo = $repository;
         $this->model = $model;
         $this->bikeController = $bikeController;
+        $this->carController = $carController;
+        $this->trailerController = $trailerController;
     }
 
     public function create(Request $request) {
@@ -48,6 +52,16 @@ class LoanableController extends RestController
                 $bikeRequest->setMethod('GET');
                 $bikeRequest->request->add($request->all());
                 return $this->bikeController->retrieve($bikeRequest, $id);
+            case 'car':
+                $carRequest = new Request();
+                $carRequest->setMethod('GET');
+                $carRequest->request->add($request->all());
+                return $this->carController->retrieve($carRequest, $id);
+            case 'trailer':
+                $trailerRequest = new Request();
+                $trailerRequest->setMethod('GET');
+                $trailerRequest->request->add($request->all());
+                return $this->trailerController->retrieve($trailerRequest, $id);
             default:
                 throw new \Exception('invalid loanable type');
         }
@@ -71,6 +85,16 @@ class LoanableController extends RestController
                 $bikeRequest->setMethod('POST');
                 $bikeRequest->request->add($request->all());
                 return $this->bikeController->update($bikeRequest, $id);
+            case 'car':
+                $carRequest = new Request();
+                $carRequest->setMethod('POST');
+                $carRequest->request->add($request->all());
+                return $this->carController->update($carRequest, $id);
+            case 'trailer':
+                $trailerRequest = new Request();
+                $trailerRequest->setMethod('POST');
+                $trailerRequest->request->add($request->all());
+                return $this->trailerController->update($trailerRequest, $id);
             default:
                 throw new \Exception('invalid loanable type');
         }
