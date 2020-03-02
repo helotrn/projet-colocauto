92ca906b266d668c050a33a5d47e340e13aed299
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-03-02 13:40:03 -0500

    Generalize UserTransformer

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -11,9 +11,22 @@ class UserTransformer extends BaseTransformer
     public function transform($item, $options = []) {
         $output = parent::transform($item, $options);
 
-        if (isset($options['context']['Community']) && $options['pivot']
-          && $this->shouldIncludeField('role', $options)) {
-            $output['role'] = $options['pivot']['role'];
+        if (isset($options['context']['Community']) && $options['pivot']) {
+            foreach ($options['pivot']->toArray() as $key => $value) {
+                if ($key === 'id') {
+                    continue;
+                }
+
+                if ($this->shouldIncludeField($key, $options)) {
+                    $output[$key] = $options['pivot'][$key];
+                }
+            }
+
+            foreach ($options['pivot']->morphOnes as $key => $target) {
+                if ($this->shouldIncludeRelation($key, $options['pivot'], $options)) {
+                    $output[$key] = $options['pivot']->{$key};
+                }
+            }
         }
 
         return $output;
