772cb8c4dc055d9fbebbb8b0e02e4931d7dea8b8
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-03-05 12:28:59 -0500

    Add account balance field to users table

---
+++ database/migrations/2020_03_05_115546_add_accout_balance_to_users.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAccoutBalanceToUsers extends Migration
+{
+    public function up() {
+        Schema::table('users', function (Blueprint $table) {
+            $table->decimal('balance', 8, 2)->default(0);
+        });
+    }
+
+    public function down() {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('balance');
+        });
+    }
+}
