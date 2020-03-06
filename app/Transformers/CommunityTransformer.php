5d39ba83a4fe48f1c296a85318dc1bc1377879b7
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-03-06 11:44:25 -0500

    Hide users in community until user is approved

--- app/Transformers/CommunityTransformer.php
+++ app/Transformers/CommunityTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers;
 
+use Auth;
 use Molotov\Transformers\BaseTransformer;
 
 class CommunityTransformer extends BaseTransformer
@@ -11,8 +12,20 @@ class CommunityTransformer extends BaseTransformer
     public function transform($item, $options = []) {
         $output = parent::transform($item, $options);
 
+        $user = Auth::user();
+        if ($user && $user->isAdmin()) {
+            return $output;
+        }
 
+        if (!$user) {
+            unset($output['users']);
+        }
 
+        $approvedCommunity = $user->communities
+            ->where('id', $item->id)
+            ->where('pivot.approved_at', '!=', null);
+        if ($approvedCommunity->isEmpty()) {
+            unset($output['users']);
         }
 
         return $output;
