8f99686c480e3b5b92798f8b73db11eb40f7a9b5
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-03-06 11:44:12 -0500

    Move repeated transformer behaviour to BaseTransformer

--- app/Transformers/CommunityTransformer.php
+++ app/Transformers/CommunityTransformer.php
@@ -6,25 +6,13 @@ use Molotov\Transformers\BaseTransformer;
 
 class CommunityTransformer extends BaseTransformer
 {
+    protected $contexts = ['User'];
+
     public function transform($item, $options = []) {
         $output = parent::transform($item, $options);
 
-        if (isset($options['context']['User']) && $options['pivot']) {
-            foreach ($options['pivot']->toArray() as $key => $value) {
-                if ($key === 'id') {
-                    continue;
-                }
 
-                if ($this->shouldIncludeField($key, $options)) {
-                    $output[$key] = $options['pivot'][$key];
-                }
-            }
 
-            foreach ($options['pivot']->morphOnes as $key => $target) {
-                if ($this->shouldIncludeRelation($key, $options['pivot'], $options)) {
-                    $output[$key] = $options['pivot']->{$key};
-                }
-            }
         }
 
         return $output;
--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -6,27 +6,5 @@ use Molotov\Transformers\BaseTransformer;
 
 class UserTransformer extends BaseTransformer
 {
-    public function transform($item, $options = []) {
-        $output = parent::transform($item, $options);
-
-        if (isset($options['context']['Community']) && $options['pivot']) {
-            foreach ($options['pivot']->toArray() as $key => $value) {
-                if ($key === 'id') {
-                    continue;
-                }
-
-                if ($this->shouldIncludeField($key, $options)) {
-                    $output[$key] = $options['pivot'][$key];
-                }
-            }
-
-            foreach ($options['pivot']->morphOnes as $key => $target) {
-                if ($this->shouldIncludeRelation($key, $options['pivot'], $options)) {
-                    $output[$key] = $options['pivot']->{$key};
-                }
-            }
-        }
-
-        return $output;
-    }
+    protected $contexts = ['Community'];
 }
