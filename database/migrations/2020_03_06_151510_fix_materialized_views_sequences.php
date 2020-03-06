cd2068e663e2da443499909a38aa9108653c503b
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-03-06 15:48:23 -0500

    Add shared sequence for loanables

---
+++ database/migrations/2020_03_06_151510_fix_materialized_views_sequences.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixMaterializedViewsSequences extends Migration
+{
+    public function up() {
+        \DB::statement('CREATE SEQUENCE IF NOT EXISTS loanables_id_seq');
+        foreach (['cars', 'bikes', 'trailers'] as $table) {
+            \DB::statement("ALTER TABLE $table ALTER COLUMN id SET DEFAULT nextval('loanables_id_seq'::regclass)");
+        }
+    }
+
+    public function down() {
+        \DB::statement('DROP SEQUENCE loanables_id_seq');
+        foreach (['cars', 'bikes', 'trailers'] as $table) {
+            \DB::statement("ALTER TABLE $table ALTER COLUMN id SET DEFAULT nextval('{$table}_id_seq'::regclass)");
+        }
+    }
+}
