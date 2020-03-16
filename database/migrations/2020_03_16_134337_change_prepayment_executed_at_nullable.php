d36a3e557307d65f5c43f413b7bce1ab122a310c
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-03-16 15:15:46 -0400

    Fix prepayment executed_at

---
+++ database/migrations/2020_03_16_134337_change_prepayment_executed_at_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
+
+class ChangePrepaymentExecutedAtNullable extends Migration
+{
+    public function __construct() {
+        if (!Type::hasType('enum')) {
+            Type::addType('enum', StringType::class);
+        }
+    }
+
+    public function up() {
+        Schema::table('pre_payments', function (Blueprint $table) {
+            $table->enum('status', ['in_process', 'canceled', 'completed'])->default('in_process')->change();
+            $table->dateTimeTz('executed_at')->nullable()->change();
+        });
+    }
+
+    public function down() {
+        Schema::table('pre_payments', function (Blueprint $table) {
+            $table->enum('status', ['in_process', 'canceled', 'completed'])->default(null)->change();
+            $table->dateTimeTz('executed_at')->nullable(false)->change();
+        });
+    }
+}
