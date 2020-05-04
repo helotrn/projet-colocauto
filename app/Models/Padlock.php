2ff39f23ce57b5fce623a3e3ed8eaef7a2e3ec9e
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-05-04 18:34:54 -0400

    Add padlock search scope + Fix padlock relations

--- app/Models/Padlock.php
+++ app/Models/Padlock.php
@@ -19,32 +19,13 @@ class Padlock extends BaseModel
         'external_id' => 'text',
         'name' => 'text',
         'loanable.name' => 'text',
+        'loanable.id' => 'number',
     ];
-
-    public static function boot() {
-        parent::boot();
-
-        self::saved(function ($model) {
-            if ($model->loanable) {
-                $newType = 'App\\Models\\' . ucfirst($model->loanable->type);
-
-                if ($model->loanable_type !== $newType) {
-                    $model->loanable_type = $newType;
-                    $model->save();
-                }
-            }
-        });
-    }
 
     protected $fillable = [
         'external_id',
         'mac_address',
         'name',
-    ];
-
-    public $morphOnes = [
-        'bike' => 'loanable',
-        'trailer' => 'loanable',
     ];
 
     public $items = ['loanable'];
@@ -53,19 +34,23 @@ class Padlock extends BaseModel
         return $this->belongsTo(Loanable::class);
     }
 
-    public function bike() {
-        return $this->morphOne(Bike::class, 'loanable');
-    }
-
-    public function trailer() {
-        return $this->morphOne(Trailer::class, 'loanable');
-    }
-
-    public function accessibleBy(Builder $query, $user) {
+    public function scopeAccessibleBy(Builder $query, $user) {
         if ($user->isAdmin()) {
             return $query;
         }
 
         return $query->where('1 = 0');
+    }
+
+    public function scopeSearch(Builder $query, $q) {
+        if (!$q) {
+            return $query;
+        }
+
+        return $query->where(
+            \DB::raw('unaccent(name)'),
+            'ILIKE',
+            \DB::raw("unaccent('%$q%')")
+        );
     }
 }
