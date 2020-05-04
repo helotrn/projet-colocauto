239cbcbd2f2272541dc8db8f1e188ed5301dde68
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-05-04 13:58:44 -0400

    Only run some seeders on local installs

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,15 +5,17 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     public function run() {
-        $this->call(ImagesTableSeeder::class);
-        $this->call(CommunitiesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(OwnersTableSeeder::class);
-        $this->call(BikesTableSeeder::class);
-        $this->call(BorrowersTableSeeder::class);
-        $this->call(LoansTableSeeder::class);
-        $this->call(PricingsTableSeeder::class);
-        $this->call(PassportSeeder::class);
+        if (app()->environment() === 'local') {
+            $this->call(ImagesTableSeeder::class);
+            $this->call(CommunitiesTableSeeder::class);
+            $this->call(UsersTableSeeder::class);
+            $this->call(OwnersTableSeeder::class);
+            $this->call(BikesTableSeeder::class);
+            $this->call(BorrowersTableSeeder::class);
+            $this->call(LoansTableSeeder::class);
+            $this->call(PricingsTableSeeder::class);
+            $this->call(PassportSeeder::class);
+        }
         $this->call(TagsTableSeeder::class);
     }
 }
