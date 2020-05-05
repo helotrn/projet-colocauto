9929348a2bf6b7683e7bf5ef6330bea59f5e44bf
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-05-05 10:46:49 -0400

    Fix failing migration

--- database/migrations/2020_05_05_104557_remove_insurance_default_value.php
+++ database/migrations/2020_05_05_104557_remove_insurance_default_value.php
@@ -4,17 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddEstimatedInsuranceToLoan extends Migration
+class RemoveInsuranceDefaultValue extends Migration
 {
     public function up() {
         Schema::table('loans', function (Blueprint $table) {
-            $table->decimal('estimated_insurance', 8, 2)->default(0);
+            $table->decimal('estimated_insurance', 8, 2)->default(null)->change();
         });
     }
 
     public function down() {
         Schema::table('loans', function (Blueprint $table) {
-            $table->dropColumn('estimated_insurance');
+            $table->decimal('estimated_insurance', 8, 2)->default(0)->change();
         });
     }
 }
