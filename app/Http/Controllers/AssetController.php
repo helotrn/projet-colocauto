3f84492e93d6576e2eddf058cea27f702df2d4df
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-05-06 15:34:16 -0400

    Properly manage image sizes

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -39,8 +39,9 @@ class AssetController extends Controller
 
         switch ($asset->type) {
             case 'image':
+                [$path, $filename] = explode('/', $rest);
                 $image = $this->imageRepo->find($newRequest, $asset->foreign_id);
-                $imageFile = $image->fetch("$image->path/$image->filename");
+                $imageFile = $image->fetch("$image->path/$filename");
                 return response($imageFile->stream())
                     ->header('Content-type', $imageFile->mime);
                 break;
@@ -92,7 +93,15 @@ class AssetController extends Controller
 
         $fullPath = "/$slug/" . $path;
 
-        return $this->model->wherePath($fullPath)->firstOrFail();
+        return $this->model
+            ->wherePath($fullPath)
+            ->where(function ($q) use ($filename) {
+                [$size, $filenameWithoutSize] = explode('_', $filename . '_', 2);
+
+                return $q->whereFilename($filename)
+                    ->orWhere('filename', preg_replace('/_$/', '', $filenameWithoutSize));
+            })
+            ->firstOrFail();
     }
 
     private function buildIntermediateRequest($token, $userAsset) {
