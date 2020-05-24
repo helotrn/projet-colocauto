4fb79394d37f26e414473fcc41a9c5dff5d949dc
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-05-24 13:12:43 -0400

    Fix lint

--- resources/app/src/views/password/Request.vue
+++ resources/app/src/views/password/Request.vue
@@ -34,8 +34,12 @@
 <i18n>
 fr:
   email: Courriel
-  instructions: Entrez l'adresse courriel associée à votre compte. Un courriel vous sera envoyé avec un lien pour réinitialiser votre mot de passe.
-  request_response: Un courriel a été envoyé à l'adresse indiquée avec des instructions pour réinitialiser le mot de passe.
+  instructions: |
+      Entrez l'adresse courriel associée à votre compte. Un courriel vous sera envoyé avec un
+      lien pour réinitialiser votre mot de passe.
+  request_response: |
+      Un courriel a été envoyé à l'adresse indiquée avec des instructions pour
+      réinitialiser le mot de passe.
   request_response_error: Une erreur s'est produite. Vérifiez l'adresse utilisée et réessayez.
   request_password: J'ai oublié mon mot de passe
   submit: Envoyer
