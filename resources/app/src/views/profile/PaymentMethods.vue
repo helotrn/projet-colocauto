c4116c6f1ab1870251e2c1862cb86871be0667e0
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-07-07 17:55:58 -0400

    Fix user payment methods view when no owner is defined
    Would never display payment methods

--- resources/app/src/views/profile/PaymentMethods.vue
+++ resources/app/src/views/profile/PaymentMethods.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="profile-payment_methods" v-if="routeDataLoaded">
-    <div v-if="user.owner">
-      <b-row>
-        <b-col class="admin__buttons">
-          <b-btn v-if="creatable" to="/profile/payment_methods/new">
-            {{ $t('ajouter une méthode de paiement') | capitalize }}
-          </b-btn>
-        </b-col>
-      </b-row>
+    <b-row>
+      <b-col class="admin__buttons">
+        <b-btn v-if="creatable" to="/profile/payment_methods/new">
+          {{ $t('ajouter une méthode de paiement') | capitalize }}
+        </b-btn>
+      </b-col>
+    </b-row>
 
-      <b-row v-if="data.length === 0">
-        <b-col>
-          Pas de méthode de paiement.
-        </b-col>
-      </b-row>
-      <b-row v-else v-for="paymentMethod in data" :key="paymentMethod.id"
-        class="profile-payment_methods__payment_methods">
-        <b-col class="profile-payment_methods__payment_methods__payment_method">
-          <router-link :to="`/profile/payment_methods/${paymentMethod.id}`">
-            {{ paymentMethod.name }}
-          </router-link>
-        </b-col>
-      </b-row>
-    </div>
-    <div v-else>
+    <b-row v-if="data.length === 0">
       <p>
         Vous pouvez ajouter une méthode de paiement pour accélérer l'ajout de crédits.
         <router-link to="/profile/payment_methods/new">
           Cliquez ici
         </router-link> pour commencer!
       </p>
-    </div>
+    </b-row>
+    <b-row v-else v-for="paymentMethod in data" :key="paymentMethod.id"
+      class="profile-payment_methods__payment_methods">
+      <b-col class="profile-payment_methods__payment_methods__payment_method">
+        <router-link :to="`/profile/payment_methods/${paymentMethod.id}`">
+          {{ paymentMethod.name }}
+        </router-link>
+      </b-col>
+    </b-row>
   </div>
   <layout-loading v-else />
 </template>
