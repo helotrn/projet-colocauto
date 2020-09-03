e6c3903769d3783e3dcb5f6eba34db26a06361c7
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-09-03 16:33:59 -0400

    Add a separator to profile sidebar
    Fix #323

--- resources/app/src/components/Profile/Sidebar.vue
+++ resources/app/src/components/Profile/Sidebar.vue
@@ -1,30 +1,34 @@
 <template>
-  <ul class="profile-sidebar" v-if="user">
-    <b-nav-item to="/profile/locomotion">
-      {{ $t('profile.titles.profile') }}
-    </b-nav-item>
-    <b-nav-item to="/profile/account">
-      {{ $t('profile.titles.account') }}
-    </b-nav-item>
-    <b-nav-item to="/profile/communities">
-      {{ $t('profile.titles.communities') }}
-    </b-nav-item>
-    <b-nav-item to="/profile/borrower">
-      {{ $t('profile.titles.borrower') }}
-    </b-nav-item>
-    <b-nav-item to="/profile/loans" :disabled="!user.borrower">
-      {{ $t('profile.titles.loans') }}
-    </b-nav-item>
-    <b-nav-item to="/profile/loanables">
-      {{ $t('profile.titles.loanables') }}
-    </b-nav-item>
-    <b-nav-item to="/profile/payment_methods" :disabled="!hasCompletedRegistration">
-      {{ $t('profile.titles.payment_methods') }}
-    </b-nav-item>
-    <b-nav-item to="/profile/invoices" :disabled="!hasCompletedRegistration">
-      {{ $t('profile.titles.invoices') }}
-    </b-nav-item>
-  </ul>
+  <div class="profile-sidebar">
+    <ul v-if="user">
+      <b-nav-item to="/profile/locomotion">
+        {{ $t('profile.titles.profile') }}
+      </b-nav-item>
+      <b-nav-item to="/profile/account">
+        {{ $t('profile.titles.account') }}
+      </b-nav-item>
+      <b-nav-item to="/profile/communities">
+        {{ $t('profile.titles.communities') }}
+      </b-nav-item>
+      <b-nav-item to="/profile/borrower">
+        {{ $t('profile.titles.borrower') }}
+      </b-nav-item>
+      <b-nav-item to="/profile/loans" :disabled="!user.borrower">
+        {{ $t('profile.titles.loans') }}
+      </b-nav-item>
+      <b-nav-item to="/profile/loanables">
+        {{ $t('profile.titles.loanables') }}
+      </b-nav-item>
+      <b-nav-item to="/profile/payment_methods" :disabled="!hasCompletedRegistration">
+        {{ $t('profile.titles.payment_methods') }}
+      </b-nav-item>
+      <b-nav-item to="/profile/invoices" :disabled="!hasCompletedRegistration">
+        {{ $t('profile.titles.invoices') }}
+      </b-nav-item>
+    </ul>
+
+    <hr>
+  </div>
 </template>
 
 <script>
@@ -38,20 +42,26 @@ export default {
 
 <style lang="scss">
 .profile-sidebar {
-  list-style: none;
-  padding: 0;
-  margin: 0;
+  hr {
+    margin: 30px 0;
+  }
 
-  .nav-item .nav-link {
-    color: $black;
-    padding-left: 0;
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
 
-    &.disabled {
-      color: $grey;
-    }
+    .nav-item .nav-link {
+      color: $black;
+      padding-left: 0;
 
-    &.router-link-exact-active, &:hover {
-      color: $locomotion-green;
+      &.disabled {
+        color: $grey;
+      }
+
+      &.router-link-exact-active, &:hover {
+        color: $locomotion-green;
+      }
     }
   }
 }
