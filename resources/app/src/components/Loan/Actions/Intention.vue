176e2c27f2d380eaa6047eed5ccd0b72b0d40b3f
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-09-03 11:17:10 -0400

    Small adjustment to insurance text on intention step

--- resources/app/src/components/Loan/Actions/Intention.vue
+++ resources/app/src/components/Loan/Actions/Intention.vue
@@ -82,8 +82,16 @@
 
         <div class="loan-actions__alert" v-if="item.loanable.type === 'car'">
           <b-alert variant="warning" show>
-            Desjardins assurances ne couvrera le trajet que s'il est bien renseigné sur
-            LocoMotion! Pensez à accepter et vérifier le pré-paiement de la réservation ici.
+            <p>
+              Desjardins assurances ne couvrera le trajet que s'il est bien renseigné sur
+              LocoMotion! Pensez à accepter et vérifier le pré-paiement de la réservation ici.
+            </p>
+
+            <p>
+              <router-link to="/assurances-desjardins">
+                Voir les conditions d'assurances
+              </router-link>
+            </p>
           </b-alert>
         </div>
       </b-collapse>
