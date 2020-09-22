5616e50a2ca54882c6a61acbd36e3021b4783310
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-09-22 14:21:21 -0400

    Add missing methods in Navbar component
    TODO Properly split Authenticated mixin

--- resources/app/src/components/Layout/Navbar.vue
+++ resources/app/src/components/Layout/Navbar.vue
@@ -165,11 +165,53 @@ export default {
     };
   },
   computed: {
+    canLoanVehicle() {
+      return this.user.borrower
+        && this.user.communities
+          .reduce((acc, c) => acc || (!!c.approved_at && !c.suspended_at), false);
+    },
+    hasCommunity() {
+      return this.isLoggedIn && this.user.communities && this.user.communities.length > 0;
+    },
+    hasCompletedRegistration() {
+      return !!this.user.submitted_at || this.canLoanVehicle;
+    },
+    isGlobalAdmin() {
+      return this.isLoggedIn && this.user.role === 'admin';
+    },
     isLoggedIn() {
       return !!this.user;
     },
+    isRegistered() {
+      const requiredFields = [
+        'name', 'last_name', 'date_of_birth',
+        'address', 'postal_code', 'phone',
+      ];
+
+      for (let i = 0, len = requiredFields.length; i < len; i += 1) {
+        if (!this.user[requiredFields[i]]) {
+          return false;
+        }
+      }
+
+      return true;
+    },
     user() {
       return this.$store.state.user;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout');
+
+      this.$store.commit('addNotification', {
+        content: "Vous n'êtes plus connecté à LocoMotion. À bientôt!",
+        title: 'Déconnexion réussie.',
+        variant: 'success',
+        type: 'logout',
+      });
+
+      this.$router.push('/');
     },
   },
   watch: {
