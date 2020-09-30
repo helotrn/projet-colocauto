424f4b4334ba6ecb5e45ad033b7bfa5b50810829
David Gendron <david.gendron@gmail.com>
2020-09-30 16:38:51 -0400

    Review conditions: content and style.

--- resources/app/src/views/Insurance.vue
+++ resources/app/src/views/Insurance.vue
@@ -1,8 +1,39 @@
 <template>
-  <layout-page name="insurance" class="insurance__content">
+  <layout-page name="insurance" class="insurance__content" padded>
     <b-row tag="section" class="page__section">
       <b-col>
-        <iframe src="/assurances.html" border="0" width="100%" />
+
+        <b-card>
+          <p>
+            Pour faciliter la lecture, nous avons simplifié le texte&nbsp;:
+          </p>
+          <ul>
+            <li>
+              <a href="/conditions-utilisation-desjardins-assurances.html">
+                Conditions d’utilisation relatives à l’assurance offerte par Desjardins Assurances
+              </a>
+            </li>
+          </ul>
+
+          <p>
+            Cependant, le texte de la loi, du règlement ou du contrat
+            d’assurance sont prioritaires s’il y a une différence.  Vous pouvez
+            accéder aux documents de références en suivant ces liens&nbsp;:
+          </p>
+          <ul>
+            <li>
+              <a href="/assurances/condition_desjardins_2_59189530_conditions_particulieres.pdf">
+                Police d’assurance
+              </a>
+            </li>
+            <li>
+              <a href="/assurances/conditions_desjardins_1_2018-09_lettre_assureur_courtier.pdf">
+                Lettre courtier
+              </a>
+            </li>
+          </ul>
+        </b-card>
+
       </b-col>
     </b-row>
   </layout-page>
@@ -15,11 +46,11 @@ export default {
 </script>
 
 <style lang="scss">
-.insurance__content {
-  iframe {
-    border: 0;
-    margin: 0;
-    height: calc(100vh - #{$layout-navbar-height});
+.page.insurance {
+  .page__background {
+    background: #f5f8fb;
+    background-image: url("/home-motif.png");
+    background-repeat: repeat;
   }
 }
 </style>
