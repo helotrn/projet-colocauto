e798c466b8e422c61f65e1b7315606cb14de5b88
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2020-11-20 17:13:01 -0500

    Add test for balance related methods and checks on user model

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public $model;
+
+    public function setUp():void {
+        parent::setUp();
+
+        $this->model = new User;
+    }
+
+    public function testUpdateBalance() {
+        $user = factory(User::class)->create();
+
+        $this->assertEquals(0, $user->balance);
+
+        $user->addToBalance(10.1);
+        $this->assertEquals(10.1, $user->balance);
+
+        $user->updateBalance(-2.8);
+        $this->assertEquals(7.3, $user->balance);
+
+        $user->updateBalance(5);
+        $this->assertEquals(12.3, $user->balance);
+
+        $user->removeFromBalance(10);
+        $this->assertEquals(2.3, $user->balance);
+    }
+
+    public function testRemoveFromBalanceBelowZero() {
+        $user = factory(User::class)->create([
+            'balance' => 1,
+        ]);
+
+        $this->assertEquals(1, $user->balance);
+        $user->removeFromBalance(1);
+
+        $this->assertEquals(0, $user->balance);
+
+        $user->balance = 1;
+        $user->save();
+
+        $this->assertEquals(1, $user->balance);
+
+        // If the balance is not sufficient, abort
+        $this->expectException("Symfony\Component\HttpKernel\Exception\HttpException");
+        $user->removeFromBalance(1.01);
+        $this->assertEquals(0, 1); // Raised above
+    }
+}
