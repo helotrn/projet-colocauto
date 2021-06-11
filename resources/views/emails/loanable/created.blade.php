d6301203446f72abfed6240140f9244eff9ff04e
David Gendron <dgendron@solon-collectif.org>
2021-06-11 14:34:56 -0400

    Give indications on how to get the car log book.
    Closes #486.

--- resources/views/emails/loanable/created.blade.php
+++ resources/views/emails/loanable/created.blade.php
@@ -10,6 +10,14 @@
 </p>
 
 <p>
+    Pour les autos en partage, allez chercher un carnet de bord chez les
+    <a href="https://mailchi.mp/solon-collectif/locomotion-comment-ca-marche#trousse%20de%20d%C3%A9part">
+      commerces participants
+    </a>
+    et déposez le dans votre coffre à gants.
+</p>
+
+<p>
     Vous recevrez un message quand une personne du voisinage voudra l'utiliser.
 </p>
 
