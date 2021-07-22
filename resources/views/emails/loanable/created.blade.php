617b9092d0ad5a3950bac03edbd130183e1e4c0d
ariannechau <ariannechau@hotmail.com>
2021-07-22 15:56:26 -0400

    update loanable templates

--- resources/views/emails/loanable/created.blade.php
+++ resources/views/emails/loanable/created.blade.php
@@ -1,31 +1,26 @@
 @extends('emails.layouts.main')
 
 @section('content')
-<p>
+<p style="text-align: justify; margin-top: 0; font-weight: 390; font-size: 17px; line-height: 24px; color: #343A40;">
     Bonjour {{ $user->name }},
 </p>
 
-<p>
+<p style="text-align: justify; margin-top: 0; font-weight: 390; font-size: 17px; line-height: 24px; color: #343A40;">
     Félicitations, votre véhicule {{ $loanable->name }} a bien été ajouté!
 </p>
 
-<p>
+<p style="text-align: justify; margin-top: 0; font-weight: 390; font-size: 17px; line-height: 24px; color: #343A40;">
     Pour les autos en partage, allez chercher un carnet de bord chez les
-    <a href="https://mailchi.mp/solon-collectif/locomotion-comment-ca-marche#trousse%20de%20d%C3%A9part">
-      commerces participants
-    </a>
+    <a href="https://mailchi.mp/solon-collectif/locomotion-comment-ca-marche#trousse%20de%20d%C3%A9part">commerces participants</a>
     et déposez le dans votre coffre à gants.
 </p>
 
-<p>
+<p style="text-align: justify; margin-top: 0; font-weight: 390; font-size: 17px; line-height: 24px; color: #343A40;">
     Vous recevrez un message quand une personne du voisinage voudra l'utiliser.
 </p>
 
-<p>
+<p style="text-align: justify; margin-top: 0; font-weight: 390; font-size: 17px; line-height: 24px; color: #343A40;">
     N'oubliez pas, en tout temps, vous pouvez modifier sa disponibilité.
 </p>
 
-<p style="text-align: right;">
-    <em>- L'équipe LocoMotion</em>
-</p>
 @endsection
