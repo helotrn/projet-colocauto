ce26603729c97712ad43181c66978c7db3b7125d
ariannechau <ariannechau@hotmail.com>
2021-07-26 14:04:57 -0400

    remove loan vehicle block for non approved users

--- resources/app/src/views/Dashboard.vue
+++ resources/app/src/views/Dashboard.vue
@@ -41,7 +41,7 @@
                 />
               </div>
 
-              <div v-if="hasTutorial('find-vehicle')">
+              <div v-if="!isGlobalAdmin && canLoanVehicle">
                 <tutorial-block
                   title="Réservez un véhicule"
                   to="/community/list"
