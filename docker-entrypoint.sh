be3f63dc795820f3cf144e8e6a77c998dba693b4
Martin Hamel <martin@komunide.com>
2021-07-28 00:42:32 -0400

    link to secrets instead of copying them

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-sleep 2
-cp $oauth_private_PATH /app/storage/oauth-private.key
-cp $oauth_public_PATH /app/storage/oauth-public.key
+ln -s $oauth_private_PATH /app/storage/oauth-private.key
+ln -s $oauth_public_PATH /app/storage/oauth-public.key
