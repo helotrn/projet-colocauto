143d46be7fda907ddb8a2b43e6a6d0a2acc39b36
Martin Hamel <martin@komunide.com>
2021-07-27 22:58:01 -0400

    Added some info logs for key debug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Str;
 use Laravel\Passport\TokenRepository;
 use Laravel\Socialite\Facades\Socialite;
 use Molotov\Traits\RespondsWithErrors;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends RestController
 {
@@ -42,6 +43,10 @@ class AuthController extends RestController
 
     public function login(LoginRequest $request)
     {
+        $public_key = file_get_contents('/app/storage/oauth-public.key');
+        $private_key = file_get_contents('/app/storage/oauth-private.key');
+        Log::info(json_encode(['public' => $public_key]));
+        Log::info(json_encode(['private' => $private_key]));
         $email = $request->get("email");
         $password = $request->get("password");
 
