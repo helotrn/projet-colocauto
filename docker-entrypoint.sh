4f37d5830f3a522b8b51ad4caccad1d6cb95f0c0
Martin Hamel <martin@komunide.com>
2021-08-01 20:14:41 -0400

    forcing creation of oauth keys links

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ln -s $oauth_private_PATH /app/storage/oauth-private.key
-ln -s $oauth_public_PATH /app/storage/oauth-public.key
+ln -s -f $oauth_private_PATH /app/storage/oauth-private.key
+ln -s -f $oauth_public_PATH /app/storage/oauth-public.key
