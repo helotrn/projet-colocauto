0d89e1db44f1a40f27b63083cb175090c1d8a181
Martin Hamel <martin@komunide.com>
2021-08-02 14:05:33 -0400

    697- added release notes

--- resources/app/src/components/Dashboard/ReleaseInfoBox.vue
+++ resources/app/src/components/Dashboard/ReleaseInfoBox.vue
@@ -1,23 +1,75 @@
 <template>
   <div class="dashboard-release-info-box">
     <b-alert
-      v-if="userInAhuntsic"
       variant="success"
       show
       class="dashboard-release-info-box_changeset"
     >
-      <h2>LocoMotion évolue</h2>
-
       <p>Nouveautés&nbsp;:</p>
       <ul>
-        <li>Ouverture au quartier Ahuntsic (nouvelle disponibilité des véhicule collectifs)</li>
-        <li>Nouvelle FAQ</li>
+        <div v-if="userInAhuntsic">
+          <li>
+            Ouverture au quartier Ahuntsic (nouvelle disponibilité des véhicules
+            collectifs)
+          </li>
+          <li>Nouvelle FAQ</li>
+          <li>
+            Si vous avez un véhicule, vous pouvez maintenant le partager à tout
+            le quartier! Par défaut, on partage votre auto ou votre vélo juste à
+            votre voisinage. Après, vous avez le choix. (Dans votre page Mes
+            Véhicules)
+          </li>
+          <li>Refonte de la page Quartier/Voisinage</li>
+        </div>
+
         <li>
-          Si vous avez un véhicule, vous pouvez maintenant le partager à tout le quartier! Par
-          défaut, on partage votre auto ou votre vélo juste à votre voisinage. Après, vous avez le
-          choix. (Dans votre page Mes Véhicules)
+          Version du 2 août 2021
+          <ul>
+            <li>Utilisateurs</li>
+            <ul>
+              <li>Ajout d'un icone sur l'onglet de l'application</li>
+              <li>
+                Retirer le bloc "réserver un véhicule" de la page d'accueil
+                quand on est en attente d'approbation
+              </li>
+              <li>Nouveau message d'information pour les emprunts</li>
+              <li>Modification de la mise en page de tous les courriels</li>
+              <li>On peut maintenant changer l'image de preuve d'adresse</li>
+              <li>
+                Au rechargement d'une page, il n'y a plus de redirection
+                automatique vers la page de connexion
+              </li>
+              <li>Nouveau look du menu de profil</li>
+              <li>
+                Dans la barre de navigation, la photo de profil de l'utilisateur
+                ou ses initiales sont affichées
+              </li>
+            </ul>
+            <li>Administrateurs</li>
+            <ul>
+              <li>
+                Dans les listes, pour les véhicules, les anciens boutons
+                "supprimer" indiquent maintenant "archiver"
+              </li>
+              <li>
+                Les liens de la section emprunts de l'administration
+                fonctionnent
+              </li>
+              <li>
+                On peut chercher des membres avec seulement une partie de
+                l'adresse courriel
+              </li>
+              <li>
+                Dans les filtres, il est maintenant possible de cliquer
+                n'importe où dans l'écran pour sortir du filtre
+              </li>
+              <li>
+                Le filtre de date sur une période d'un mois affiche maintenant
+                tous les emprunts du mois
+              </li>
+            </ul>
+          </ul>
         </li>
-        <li>Refonte de la page Quartier/Voisinage</li>
       </ul>
     </b-alert>
   </div>
@@ -29,6 +81,7 @@ import UserMixin from "@/mixins/UserMixin";
 export default {
   name: "DashboardReleaseInfoBox",
   mixins: [UserMixin],
+  components: {},
   computed: {
     /*
       Check if user is approved in Ahuntsic or any of it's neighborhoods.
@@ -51,8 +104,8 @@ export default {
       }
 
       return false;
-    },
-  },
+    }
+  }
 };
 </script>
 
