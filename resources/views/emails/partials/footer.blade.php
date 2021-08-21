5e61628246afd820176593ab343a4bcf1a828666
Martin Hamel <martin@komunide.com>
2021-08-21 15:15:21 -0400

    703- fixed styles typo

--- resources/views/emails/partials/footer.blade.php
+++ resources/views/emails/partials/footer.blade.php
@@ -1,12 +1,15 @@
 <tr style="height: 263.27px">
-    <td style="
+    <td
+        style="
         background-position: bottom center;
         background-repeat: no-repeat;
         background-color: #1e4847;
         height: 500px;
-        background-image: url('{{ env('BACKEND_URL_FROM_BROWSER') . '/mail-footer-bg.png' }}');    
-    ">
-        <p style="
+        background-image: url('{{ env('BACKEND_URL_FROM_BROWSER') }}/mail-footer-bg.png');    
+    "
+    >
+        <p
+            style="
                 position: relative;
                 top: -80px;
                 margin: 0;
@@ -15,10 +18,12 @@
                 font-size: 13px;
                 line-height: 16px;
                 text-align: center;
-            ">
+            "
+        >
             Envoyé par l'équipe LocoMotion
         </p>
-        <p style="
+        <p
+            style="
                 position: relative;
                 top: -80px;
                 margin: 0;
@@ -27,7 +32,8 @@
                 font-size: 13px;
                 line-height: 16px;
                 text-align: center;
-            ">
+            "
+        >
             info@locomotion.app
         </p>
     </td>
