43cc44ca722a968732a73bc8fb032f744942d5f0
Martin Hamel <martin@komunide.com>
2021-08-21 14:12:29 -0400

    730- encoding image inline for mails

--- resources/views/emails/partials/header.blade.php
+++ resources/views/emails/partials/header.blade.php
@@ -2,35 +2,7 @@
     <td align="center" style="height: 60px">
         <div style="text-align: center">
             <img
-                src="data:image/jpg;base64,iVBORw0KGgoAAAANSUhEUgAAAH0AAAARCAYAAADwtqMEAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA
-AXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAXrSURBVHgB3VldchpHEO7ZRURKJTK+ATpBpBME
-ncAQZMVID4ETgE8gdAIpJxB5iEFJ8MonED6B8AlETmBVKlUmLmkm3TPsTs/sLhEyJmV1Feyy2zPT
-P9PdXw8C/nhVBSlKEKgb2Du4gC+d+v0yFMUl/PVhB1qtm0/m+1wU4fpTvDYaE1gxBSCCCEJxpq+P
-gYpQwe8yfPNVcz6faBu+9W1YNQ3PjzDQrlGGa/i9X4EVUwCPjZQyTgyDZ//BWZ0NWL7TyZH0oWyS
-RUpVk/tA/AQrpsfndAi+0xcFldwoGvabQFGu2Wf8yyJaM8CyQZ+iOMrkUepNci/ECFZMj8/pQqd3
-Q2GO0cF5XoFV0/NGF5SsgVS78MOPv8CK6XE5/bdf3VRN0e6nWBP9/FkZzs5KsGoi0Py8MYL/gQqw
-KJHRKIJ07UTUD4j6QVzAR3U8F4katIzgSTXNOKCoHIGUP2d2DYYfI5LqX7LOCNd5mbtOIMqpZ0Vo
-4neX8aRrqAFzI7iXvjDRvFn6ksxCnLAnFXg9uAShWlBjvNQxBUFb39/hPFnOp41Y+rqN2aAJ8SZV
-MEb5T3Ozw3CAgJxtaC6zEN143GKRHg3aulZRBMWOM9cmGvcKXp9ngxKKQHoP0LHjwERirDwnMorh
-b3rrVDXijc5z0nZmqm4nkWyivplmyQFzhLJT+oKZg9o9P4sU8J0APldZj70Fl0+Jbf3cfMqpdWne
-zY0rdHgXeFaiuZXqaedmU4XNW3FkpnFkV1jE6SSIhFMm+Q1OPDIRqCUqYZ06TRmCfodhZAWIx+mI
-yV5HYAvp8o/tOkhkjEyQxkGZepvItbne0bccWClloyULzGmwp43u6ZuQ6fN5aVByOf1+UWeLsp0X
-9dc2SKiJ8nXnT6Lt1XPGzQLs/k4vsrRFBgumW7D3Yhdn2MEdOJsYDbymOs4487ucCB+Pq7/YQucd
-Y3p/561zlDjc8u9AvfHUcVQWSBNxxKLCgWRyYEqPtHMqdrxkGzgrQ/D5UU5aX+urtsBuWPc8YP9w
-rMGZpR7qKRaq3WYzVxM9aD7Snz4gXzLO9lwscifJxi0IP+wmATNrZxdI76qS3IZYI2uzUyyqVeKu
-lbwTfn/MokgiX42dfhGK3TtwN4li6dHnD6cdX4GENIiLsYIYQ+1wzCITs9QGj56efs+dxw1oNkjM
-O0GHd5N3pK9UVt9lt3x8A0qv3tcPTq1OqOuTje9zZ9k/NIGo7SdsNoaFnJ7Uh4kDSvTEh+P8YSxN
-7c/hs/ylXP4MBRLiIC7OHgqNZqlpeZPndn7nZG69xOSZgE88jYuMmrwsUiJdLlRctpBu1VN4AC0S
-6bEA5Zw2yJBvJMV+Z9VhP0XN4zfrlj15Yqqk5qAoUR4qp4OReNMq+Sd7wZw+vWH82ykZw2A7tday
-KGB6BRkAUwgb3eJha7tOj48Ps44ReT1dE1HyzpxAWTSp5BtnTr4ziS8eR4YcDiL4dr03lz/uvSO8
-0roJYZvoqsLSrLARLHx5GBiVgoMcO54yCk+jmxtnjr4AFt8s+0QtYHoJBF7DV6b8kb2iwQk7fJo8
-tM8PvAUvnU+BpcQQ+8M4uqh1oNZpOFCaj9e/wHNGYXoKvHbqcf33aMj3QIAlEE/m8ofhlV5H4jVp
-hwioOambgTikv6fMmf/0WFbwDCUtnwLvYMeZv+rqy4Dmsk/UKAspPLtIKDjR65K9JHSSx9R3P5Du
-n95JmEASMp3kcOB7RJqpet+60c+dcSIfdWbyc9LI3F2Hgzh6z/8qpfkkGCP6hlr7aOegDcXTuN4c
-DLClxECHh6oGn4MI3DqOTy1+/CmbraDbAJnjhAKiXE4E2KL+Lh42NHVtMSCG2pR3OkJrOf9LGwdt
-6d5XYfsk9KEF8sq3+oTpfvwTnfqz1iHn3a6byAyD67QeOOYOkW7dMxTNM+y3cFOU9W//f/V6o4f6
-jvDUrKPLR6yvwGxWzzF6AbNMLEveKV9BXsDtLN7WcnjI8cM+ZiLxzB7goL2UmHN8Sy0wZUffBrPn
-BUHZFf4F7kG+s7LDbT4AAAAASUVORK5CYII="
+                src="data:image/png;base64,<?php echo base64_encode(file_get_contents('./public/mail-header-logo.png')) ?>"
                 alt="LocoMotion"
                 style="width: 125px; height: 17px"
             />
