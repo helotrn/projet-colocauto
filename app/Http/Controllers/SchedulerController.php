3db402cd1fa18e601dc172ef4f18fc222e4b463d
Martin Hamel <martin@komunide.com>
2021-08-26 10:42:47 -0400

    740- Scheduler controller calls the code directly for Noke without going through commands

--- app/Http/Controllers/SchedulerController.php
+++ app/Http/Controllers/SchedulerController.php
@@ -5,31 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Http\Response;
+use App\Services\NokeService;
+use GuzzleHttp\Client;
+use App\Console\Commands\NokeSyncLoans;
+use App\Console\Commands\NokeSyncLocks;
+use App\Console\Commands\NokeSyncUsers;
 
 class SchedulerController extends Controller
 {
-    public static function nokeLocks($appKey)
+    public static function nokeLocks($appKey, Client $client, NokeService $service)
     {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        Artisan::call("noke:sync:locks");
+        $command = new NokeSyncLocks($client, $service);
+        $command->handle();
     }
 
-    public static function nokeUsers($appKey)
+    public static function nokeUsers($appKey, Client $client, NokeService $service)
     {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        Artisan::call("noke:sync:users");
+        $command = new NokeSyncUsers($client, $service);
+        $command->handle();
     }
 
-    public static function nokeLoans($appKey)
+    public static function nokeLoans($appKey, Client $client, NokeService $service)
     {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        Artisan::call("noke:sync:loans");
+        $command = new NokeSyncLoans($client, $service);
+        $command->handle();
     }
     public static function actionsComplete($appKey)
     {
