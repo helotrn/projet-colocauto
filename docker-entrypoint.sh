d1d1b8da079dd7c3326a40a2492b104db16be104
Martin Hamel <martin@komunide.com>
2021-09-03 14:08:15 -0400

    742- chmod on /app/storage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,7 @@ ln -s -f $OAUTH_PRIVATE_PATH /app/storage/oauth-private.key
 ln -s -f $OAUTH_PUBLIC_PATH /app/storage/oauth-public.key
 chown -h 1000.1000 /app/storage/*key
 
+chown -R 1000.1000 /app/storage
+
 php artisan migrate --force
 php artisan queue:work & 
