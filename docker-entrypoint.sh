6e34383d1d3882e1df4032b5b63045bc58f97d71
Martin Hamel <martin@komunide.com>
2021-09-03 00:07:04 -0400

    742- moved backend to nginx

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 ln -s -f $OAUTH_PRIVATE_PATH /app/storage/oauth-private.key
 ln -s -f $OAUTH_PUBLIC_PATH /app/storage/oauth-public.key
+
+composer install
+php artisan migrate --force
+php artisan queue:work & 
