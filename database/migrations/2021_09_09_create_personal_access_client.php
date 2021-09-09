ab348e36bbb2cd6d879090678645cfc482b7df0e
Martin Hamel <martin@komunide.com>
2021-09-09 12:59:28 -0400

    607 added migration to create PersonalAccessClient

---
+++ database/migrations/2021_09_09_create_personal_access_client.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
+
+
+class PersonalAccessClient extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call("passport:client", ["--personal" => true, "--quiet" => true]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
