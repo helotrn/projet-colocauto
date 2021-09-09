efc6f22de13caea0d528d32ce86c2678172a44ce
Martin Hamel <martin@komunide.com>
2021-09-09 14:17:30 -0400

    607 prettier

--- resources/app/src/views/admin/Users.vue
+++ resources/app/src/views/admin/Users.vue
@@ -14,11 +14,7 @@
     <b-row>
       <b-col class="admin__selection">
         <b-button-group variant="info">
-          <b-dropdown
-            left
-            :disabled="selected.length === 0"
-            text="Actions groupées"
-          >
+          <b-dropdown left :disabled="selected.length === 0" text="Actions groupées">
             <b-dropdown-item @click="sendPasswordResetEmail">
               Courriel de réinit. de mot de passe
             </b-dropdown-item>
@@ -91,11 +87,7 @@
           </template>
           <template v-slot:cell(actions)="row">
             <div class="user-actions">
-              <admin-list-actions
-                :columns="['edit', 'delete']"
-                :row="row"
-                :slug="slug"
-              />
+              <admin-list-actions :columns="['edit', 'delete']" :row="row" :slug="slug" />
               <b-button
                 :id="'mandate-' + row.item.id"
                 size="sm"
@@ -130,11 +122,7 @@
       </b-col>
 
       <b-col md="6">
-        <admin-pagination
-          :params="contextParams"
-          :total="total"
-          @change="setParam"
-        />
+        <admin-pagination :params="contextParams" :total="total" @change="setParam" />
       </b-col>
     </b-row>
   </b-container>
