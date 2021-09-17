56a3954856f61d7198ef3f1c1da15ee022029356
Martin Hamel <martin@komunide.com>
2021-09-17 15:39:27 +0000

    added a new phpunit script that sets the right database

--- psql/init/initdb-postgis.sh
+++ psql/init/initdb-postgis.sh
@@ -5,3 +5,7 @@ psql -U locomotion -d locomotion -c "CREATE EXTENSION IF NOT EXISTS unaccent;"
 psql -U locomotion -d locomotion -c "CREATE EXTENSION IF NOT EXISTS citext;"
 
 psql -U locomotion -c 'create database locomotion_test;'
+
+psql -U locomotion -d locomotion_test -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql -U locomotion -d locomotion_test -c "CREATE EXTENSION IF NOT EXISTS unaccent;"
+psql -U locomotion -d locomotion_test -c "CREATE EXTENSION IF NOT EXISTS citext;"
