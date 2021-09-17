97f4ca2fb9daea7803c35eb682f9fceba051d258
Martin Hamel <martin@komunide.com>
2021-09-17 15:21:39 +0000

    a test database will be automatically created

--- psql/init/initdb-postgis.sh
+++ psql/init/initdb-postgis.sh
@@ -4,3 +4,4 @@ psql -U locomotion -d locomotion -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 psql -U locomotion -d locomotion -c "CREATE EXTENSION IF NOT EXISTS unaccent;"
 psql -U locomotion -d locomotion -c "CREATE EXTENSION IF NOT EXISTS citext;"
 
+psql -U locomotion -c 'create database locomotion_test;'
