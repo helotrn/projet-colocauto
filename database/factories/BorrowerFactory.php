db6648b0607e9a0c65c82b1175c7dd94becacb6b
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2021-09-24 11:17:34 -0400

    752 Create relations required by the Loan factory
    Also add various states to easily generate a completely valid loan

--- database/factories/BorrowerFactory.php
+++ database/factories/BorrowerFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Models\Borrower;
-use Illuminate\Support\Str;
+use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(Borrower::class, function (Faker $faker) {
@@ -15,4 +15,11 @@ $factory->define(Borrower::class, function (Faker $faker) {
         "submitted_at" => date("Y-m-d"),
         "approved_at" => null,
     ];
+});
+
+$factory->afterMaking(Borrower::class, function ($borrower) {
+    if (!$borrower->user_id) {
+        $user = factory(User::class)->create();
+        $borrower->user_id = $user->id;
+    }
 });
--- database/factories/OwnerFactory.php
+++ database/factories/OwnerFactory.php
@@ -1,13 +1,20 @@
 <?php
 
 use App\Models\Owner;
+use App\Models\User;
 use Carbon\Carbon;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
 
 $factory->define(Owner::class, function (Faker $faker) {
     return [
         "submitted_at" => Carbon::now(),
         "approved_at" => Carbon::now(),
     ];
+});
+
+$factory->afterMaking(Owner::class, function ($owner) {
+    if (!$owner->user_id) {
+        $user = factory(User::class)->create();
+        $owner->user_id = $user->id;
+    }
 });
