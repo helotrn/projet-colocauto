e3740b789443f3a3aece3d2267a5050ac5c89f6e
Martin Hamel <martin@komunide.com>
2021-09-30 13:47:39 +0000

    457- removed trailing && in startup script

--- start_php_container.sh
+++ start_php_container.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 
 # putting the secrets at the laravel designated place
-ln -s -f $OAUTH_PRIVATE_PATH '/var/www/html/storage/oauth-private.key' && \
-ln -s -f $OAUTH_PUBLIC_PATH '/var/www/html/storage/oauth-public.key' && \
+ln -s -f $OAUTH_PRIVATE_PATH '/var/www/html/storage/oauth-private.key' 
+ln -s -f $OAUTH_PUBLIC_PATH '/var/www/html/storage/oauth-public.key' 
 
 # migrate need the cache but the cache table need to exist first 
 # so using the array cache for the migration
-export CACHE_DRIVER_OLD=$CACHE_DRIVER && \
-export CACHE_DRIVER=array && \
-php artisan migrate --force && \
-export CACHE_DRIVER=$CACHE_DRIVER_OLD && \
+export CACHE_DRIVER_OLD=$CACHE_DRIVER 
+export CACHE_DRIVER=array 
+php artisan migrate --force 
+export CACHE_DRIVER=$CACHE_DRIVER_OLD 
 
 # starting the worker for the queue 
 php artisan queue:work & \
