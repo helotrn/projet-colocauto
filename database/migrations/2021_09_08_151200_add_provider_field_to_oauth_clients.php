28c31efb27bbfb798de1b6b42b743a33113ff625
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2021-10-01 09:39:43 -0400

    Apply prettier

--- database/migrations/2021_09_08_151200_add_provider_field_to_oauth_clients.php
+++ database/migrations/2021_09_08_151200_add_provider_field_to_oauth_clients.php
@@ -9,17 +9,17 @@ class AddProviderFieldToOauthClients extends Migration
     public function up()
     {
         Schema::table("oauth_clients", function (Blueprint $table) {
-			if (!Schema::hasColumn('oauth_clients', 'provider')) {
-				$table
-					->string("provider")
-					->nullable()
-					->default(null);
-			}
+            if (!Schema::hasColumn("oauth_clients", "provider")) {
+                $table
+                    ->string("provider")
+                    ->nullable()
+                    ->default(null);
+            }
         });
     }
 
     public function down()
     {
-		// Do not remove, it should have been there from the beginning
+        // Do not remove, it should have been there from the beginning
     }
 }
