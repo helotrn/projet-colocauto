1bc25e91c6fd95882a36a9c3e5545e6102048f49
Alex Toulemonde <alexandre.toulemonde@gmail.com>
2021-11-10 12:50:06 -0500

    Removed approved local template

--- resources/views/emails/borrower/approved_text.blade.php
+++ resources/views/emails/borrower/approved_text.blade.php
@@ -1,14 +1,1 @@
-@extends('emails.layouts.main_text')
-
-@section('content')
-Bonjour {{ $user->name }},
-
-Félicitations, votre dossier de conduite est approuvé!
-
-Vous pouvez maintenant emprunter les autos de vos voisins et voisines ;-)
-
-Voir mon voisinage [{{ env('FRONTEND_URL') . '/community/list' }}]
-
-L'équipe LocoMotion
-info@locomotion.app
-@endsection
+Replaced by Mandrill template
