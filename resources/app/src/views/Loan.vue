57f617dcff784fd6f1de2c124868938100a5f1b6
David Gendron <dgendron@solon-collectif.org>
2021-11-26 15:46:34 -0500

    227 Add message telling that one must cancel in order to modify a loan.

--- resources/app/src/views/Loan.vue
+++ resources/app/src/views/Loan.vue
@@ -18,6 +18,16 @@
             @incident="addIncident('accident')"
           />
 
+          <b-alert
+            show
+            variant="warning"
+            v-if="!!item.id && item.loan_status === 'in_process' && !loanIsCanceled"
+          >
+            <h4>Modification de la réservation</h4>
+            <strong>Pour modifier</strong> cette demande de réservation, merci de l'annuler et d'en
+            créer une nouvelle.
+          </b-alert>
+
           <loan-actions
             :item="item"
             @load="loadItemAndUser"
