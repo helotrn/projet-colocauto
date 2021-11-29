2389e1a15a5ae09081b247103f44045353d88c6e
Alex Toulemonde <alexandre.toulemonde@gmail.com>
2021-11-29 11:28:43 -0500

    Revert "Removed approved local template"
    This reverts commit 1bc25e91c6fd95882a36a9c3e5545e6102048f49.

--- resources/views/emails/borrower/approved_text.blade.php
+++ resources/views/emails/borrower/approved_text.blade.php
@@ -1,1 +1,14 @@
-Replaced by Mandrill template
+@extends('emails.layouts.main_text')
+
+@section('content')
+Bonjour {{ $user->name }},
+
+Félicitations, votre dossier de conduite est approuvé!
+
+Vous pouvez maintenant emprunter les autos de vos voisins et voisines ;-)
+
+Voir mon voisinage [{{ env('FRONTEND_URL') . '/community/list' }}]
+
+L'équipe LocoMotion
+info@locomotion.app
+@endsection
