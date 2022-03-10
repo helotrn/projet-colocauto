f9926369f9b513d0406cc9dd24b5a5751119aeb8
David Gendron <dgendron@solon-collectif.org>
2022-03-10 16:07:27 -0500

    906 Comment about how the currently logged-in user gets updated when change it's profile.

--- resources/app/src/store/models/users.js
+++ resources/app/src/store/models/users.js
@@ -197,6 +197,9 @@ export default new RestModule(
           cancelToken: cancelToken.token,
         });
 
+        // If the user currently being updated is the logged-in user
+        // (rootState.user), then update it's state as well.
+        // Only diff with the update method in RestModule.
         if (rootState.user.id === item.id) {
           commit("user", { ...item }, { root: true });
         }
