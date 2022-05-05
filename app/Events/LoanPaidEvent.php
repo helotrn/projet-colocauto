bb092477e930819bb62bb0c852cda94a168e9b22
ariannechau <ariannechau@hotmail.com>
2022-05-05 13:14:25 -0400

    861- replace html tag by text

--- app/Events/LoanPaidEvent.php
+++ app/Events/LoanPaidEvent.php
@@ -11,14 +11,14 @@ use Illuminate\Queue\SerializesModels;
 
 class LoanPaidEvent extends SendInvoiceEmailEvent
 {
-    public static $defaultText = <<<HTML
+    public static $defaultText = <<<TEXT
 Cela signifie que vous ne pouvez plus modifier les informations du trajet (km départ,
 km retour, achat carburant). Si vous vous rendez compte d’une erreur, dites-le à la personne
 qui vous a prêté son auto ou vélo et contactez info@locomotion.app, on vous aidera à
 ajuster vos factures.
 
 Merci d'avoir utilisé LocoMotion!
-HTML;
+TEXT;
     public function __construct(
         User $user,
         array $invoice,
