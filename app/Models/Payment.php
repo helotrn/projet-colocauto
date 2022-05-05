7c5131383d04412b7ad53058a43857e7a356d550
David Gendron <dgendron@solon-collectif.org>
2022-05-05 14:18:10 -0400

    950 Add Payment::complete() and Payment::isCompleted().

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -68,4 +68,17 @@ class Payment extends Action
     {
         return $this->belongsTo(Loan::class);
     }
+
+    public function complete($at = null)
+    {
+        $this->executed_at = new Carbon($at);
+        $this->status = "completed";
+
+        return $this;
+    }
+
+    public function isCompleted()
+    {
+        return $this->status == "completed";
+    }
 }
