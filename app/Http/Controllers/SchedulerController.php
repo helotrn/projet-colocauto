79e18b4e03540324c4e604b3a8226211ed6b908f
David Gendron <dgendron@solon-collectif.org>
2022-05-23 13:57:48 -0400

    964 Call artisan commands using Artisan.

--- app/Http/Controllers/SchedulerController.php
+++ app/Http/Controllers/SchedulerController.php
@@ -4,77 +4,60 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Http\Response;
-use App\Services\NokeService;
-use GuzzleHttp\Client;
-use App\Console\Commands\NokeSyncLoans;
-use App\Console\Commands\NokeSyncLocks;
-use App\Console\Commands\NokeSyncUsers;
-use App\Console\Commands\ActionsComplete;
-use App\Console\Commands\EmailLoanUpcoming;
-use App\Console\Commands\EmailLoanPrePaymentMissing;
 
 class SchedulerController extends Controller
 {
-    public static function nokeLocks(
-        $appKey,
-        Client $client,
-        NokeService $service
-    ) {
+    public static function nokeLocks($appKey)
+    {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        $command = new NokeSyncLocks($client, $service);
-        $command->handle();
+
+        Artisan::call("noke:sync:locks");
     }
 
-    public static function nokeUsers(
-        $appKey,
-        Client $client,
-        NokeService $service
-    ) {
+    public static function nokeUsers($appKey)
+    {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        $command = new NokeSyncUsers($client, $service);
-        $command->handle();
+
+        Artisan::call("noke:sync:users");
     }
 
-    public static function nokeLoans(
-        $appKey,
-        Client $client,
-        NokeService $service
-    ) {
+    public static function nokeLoans($appKey)
+    {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        $command = new NokeSyncLoans($client, $service);
-        $command->handle();
+
+        Artisan::call("noke:sync:loans");
     }
-    public static function actionsComplete(
-        $appKey,
-        ActionController $controller
-    ) {
+
+    public static function actionsComplete($appKey)
+    {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        $command = new ActionsComplete($controller);
-        $command->handle();
+
+        Artisan::call("actions:complete");
     }
+
     public static function emailLoanUpcoming($appKey)
     {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        $command = new EmailLoanUpcoming();
-        $command->handle();
+
+        Artisan::call("email:loan:upcoming");
     }
+
     public static function emailPrePayement($appKey)
     {
         if ($appKey != env("APP_KEY")) {
             return new Response("Wrong app key", 403);
         }
-        $command = new EmailLoanPrePaymentMissing();
-        $command->handle();
+
+        Artisan::call("email:loan:pre_payment_missing");
     }
 }
