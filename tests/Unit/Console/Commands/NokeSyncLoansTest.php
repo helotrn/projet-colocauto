2ee9715649355f20af6e931da6a3a4e50dbb7b97
David Gendron <dgendron@solon-collectif.org>
2022-08-01 13:32:44 -0400

    937 Corrections to unit tests.

--- tests/Unit/Console/Commands/NokeSyncLoansTest.php
+++ tests/Unit/Console/Commands/NokeSyncLoansTest.php
@@ -70,7 +70,7 @@ class NokeSyncLoansTest extends TestCase
                 "loanable_id" => $bikeWithPadlock,
             ]);
 
-        // This one should remain accessible
+        // Loan ended more than 15 minutes ago, should not have access.
         $loanEndedEarlier = factory(Loan::class)
             ->states("withAllStepsCompleted")
             ->create([
@@ -105,48 +105,5 @@ class NokeSyncLoansTest extends TestCase
             [$loanStartingSoon->id, $loanInProcess->id, $loanEndedRecently->id],
             $testLoanIds
         );
-    }
-
-    // Mettre dans l<autre paquet, ou tout s/parer,
-    // TestGetLockUsers
-    public function testGetLockUsers_PadlockAccessibleDelay()
-    {
-        $now = CarbonImmutable::now();
-        CarbonImmutable::setTestNow($now);
-
-        $bikeWithPadlock = factory(Bike::class)
-            ->states("withCommunity", "withPadlock")
-            ->create();
-
-        // This one should not be accessible anymore
-        $loanEndingBeforeDelay = factory(Loan::class)
-            ->states("withAllStepsCompleted")
-            ->create([
-                "departure_at" => $now->subMinutes(120),
-                "duration_in_minutes" => 100,
-                "loanable_id" => $bikeWithPadlock,
-            ]);
-
-        // This one should remain accessible
-        $loanEndingAfterDelay = factory(Loan::class)
-            ->states("withAllStepsCompleted")
-            ->create([
-                "departure_at" => $now->subMinutes(120),
-                "duration_in_minutes" => 110,
-                "loanable_id" => $bikeWithPadlock,
-            ]);
-
-        $query = NokeSyncLoansCommand::getLoansFromPadlockMacQuery([
-            "mac_address" => $bikeWithPadlock->padlock->mac_address,
-        ]);
-
-        $loans = $query->get();
-
-        $testLoanIds = [];
-        foreach ($loans as $loan) {
-            $testLoanIds[] = $loan->id;
-        }
-
-        $this->assertEquals([$loanEndingAfterDelay->id], $testLoanIds);
     }
 }
