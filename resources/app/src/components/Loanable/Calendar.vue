585ce2aea98f4059d7c0474ebed36282446fa235
David Gendron <dgendron@solon-collectif.org>
2022-11-16 16:39:17 -0500

    1076 Create vanilla event calendar.

---
+++ resources/app/src/components/Loanable/Calendar.vue
@@ -0,0 +1,29 @@
+<template>
+  <vue-cal
+    :events="events"
+    locale="fr"
+    @ready="$emit('ready', $event)"
+    @view-change="$emit('view-change', $event)"
+  >
+  </vue-cal>
+</template>
+
+<script>
+import VueCal from "vue-cal";
+import "vue-cal/dist/i18n/fr";
+
+export default {
+  name: "Calendar",
+  props: {
+    events: {
+      type: Array,
+    },
+  },
+  components: { VueCal },
+  data() {
+    return {
+      events: [],
+    };
+  },
+};
+</script>
