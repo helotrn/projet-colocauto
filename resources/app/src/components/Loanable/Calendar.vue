d812b4dcb550fa08fa7a1675eaf3d12604c5b89c
David Gendron <dgendron@solon-collectif.org>
2022-11-17 14:42:24 -0500

    1076 Improve styling of time axis.

--- resources/app/src/components/Loanable/Calendar.vue
+++ resources/app/src/components/Loanable/Calendar.vue
@@ -3,12 +3,26 @@
     :class="classList"
     :disable-views="['years', 'year']"
     :defaultView="defaultView"
+    :time-step="15"
+    :time-cell-height="18"
     :events="events"
     locale="fr"
     :xsmall="variant == 'small'"
     @ready="$emit('ready', $event)"
     @view-change="$emit('view-change', $event)"
   >
+    <template v-slot:time-cell="{ hours, minutes }">
+      <div
+        :class="{
+          'loanable-calendar__time-step--hours': !minutes,
+          'loanable-calendar__time-step--minutes': minutes,
+        }"
+      >
+        <span class="line"></span>
+        <span v-if="!minutes">{{ hours }}</span>
+        <span v-else>{{ minutes }}</span>
+      </div>
+    </template>
   </vue-cal>
 </template>
 
@@ -49,6 +63,19 @@ export default {
 <style lang="scss">
 .loanable-calendar {
   &.loanable-calendar--small {
+  }
+
+  // Styling the time axis.
+  // Specificity seems necessary here.
+  .vuecal__time-column .loanable-calendar__time-step--hours .line::before {
+    border-color: $locomotion-green;
+  }
+  .loanable-calendar__time-step--hours {
+    color: $locomotion-green;
+    font-weight: bold;
+  }
+  .loanable-calendar__time-step--minutes {
+    font-size: 0.7em;
   }
 }
 </style>
