495be326a21fade6d82b877bf6089c021854ca67
Fabien Quatravaux <fabien.quatravaux@riseup.net>
2023-01-05 14:26:01 +0100

    [dev] Improve expense info box display and style

--- resources/app/src/components/Expenses/InfoBox.vue
+++ resources/app/src/components/Expenses/InfoBox.vue
@@ -2,28 +2,39 @@
   <b-card no-body class="expense-info-box shadow my-2" bg="white" no-body :class="{ disabled: loading }">
     <router-link :to="`/wallet/expenses/${id}`">
       <div class="card-header">
-          <b-row><span class="badge badge-primary">Emprunt</span></b-row>
-          <b-row><small v-if="executed_at">{{ new Date(executed_at).toLocaleDateString() }}</small></b-row>
+          <b-row>
+            <b-col><span class="badge badge-primary">Emprunt</span></b-col>
+            <b-col class="text-right"><icons-edit class="svgicon action" /></b-col>
+          </b-row>
+          <b-row>
+            <b-col><span class="small" v-if="executed_at">{{ new Date(executed_at).toLocaleDateString() }}</span></b-col>
+          </b-row>
       </div>
       <div class="card-body">
         <b-row>
           <b-col>
-            {{ name }}<br/>
-            <small>par {{ user.full_name }}</small>
+            <strong class="big">{{ name }}</strong><br/>
+            <span class="small">par {{ user.full_name }}</span>
           </b-col>
-          <strong>{{ amount }} €</strong>
+          <b-col class="text-right">
+            <strong class="huge">{{ amount }} €</strong>
+          </b-col>
         </b-row>
-        <b-row><b-col>{{ loanable.name }}</b-col></b-row>
+        <b-row style="line-height: 1.3em">
+          <b-col class="small mt-3"><icons-car class="svgicon" /> {{ loanable.name }}</b-col>
+        </b-row>
       </div>
     </router-link>
-    <div class="card-footer">
-      <span class="badge badge-warning">2 modifications effectuées</span>
+    <div class="card-footer text-right">
+      <span class="badge badge-warning small">2 modifications effectuées</span>
     </div>
   </b-card>
 </template>
 
 <script>
 import { extractErrors } from "@/helpers";
+import IconsCar from "@/assets/icons/car.svg";
+import IconsEdit from "@/assets/icons/edit.svg";
 
 export default {
   name: "ExpenseInfoBox",
@@ -37,7 +48,7 @@ export default {
       required: true,
     },
     amount: {
-      type: Number,
+      type: String,
       required: true,
     },
     executed_at: {
@@ -58,6 +69,10 @@ export default {
       loading: false,
     };
   },
+  components: {
+    IconsCar,
+    IconsEdit,
+  }
 }
 </script>
 
@@ -69,6 +84,54 @@ export default {
   a:focus {
     text-decoration: none;
     color: black;
+  }
+
+  .card-header, .card-footer {
+    background: transparent;
+  }
+  .card-header {
+    padding-top: 20px;
+    padding-bottom: 5px;
+  }
+  .card-body {
+    padding-top: 5px;
+  }
+
+  .svgicon {
+    padding: 0.15em;
+    width: 1.3em;
+    height: 1.3em;
+    vertical-align: bottom;
+  }
+
+  a .action {
+    fill: #A9AFB5;
+  }
+  a:hover .action {
+    fill: $primary;
+  }
+
+  .badge {
+    font-size: 100%;
+    font-weight: normal;
+    border-radius: 2px;
+  }
+  .badge-primary {
+    background-color: #DFEAFF;
+    color: #246AEA;
+  }
+  .badge-warning {
+    background-color: #FFEFC8;
+    color: #7E4C02;
+  }
+  .small {
+    font-size: 87.5%;
+  }
+  .big {
+    font-size: 112.5%;
+  }
+  .huge {
+    font-size: 137.5%;
   }
 }
 </style>
