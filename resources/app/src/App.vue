cb1ccbff5e7d695dbf3af68baf02ad4b2bf34772
Konstantin Fedorov <kfedorov@solon-collectif.org>
2023-01-10 14:28:12 -0500

    791 - Add notification when terms need to be re-approved

--- resources/app/src/App.vue
+++ resources/app/src/App.vue
@@ -4,14 +4,52 @@
       <p class="mb-0 text-center">👋 Vous consultez actuellement une page de {{ environment }}, pour vous rendre sur l'application Coloc'Auto c'est par ici : <a href="https://app.colocauto.org">app.colocauto.org</a></p>
     </aside>
     <router-view />
+    <b-toast
+      toast-class="terms-toast"
+      variant="warning"
+      visible
+      v-if="showTermsChanged"
+      no-auto-hide
+      no-close-button
+      solid
+      :title="$t('terms_changed_title')"
+    >
+      <p>{{ $t("terms_changed_text") }}</p>
+
+      <dl>
+        <template v-for="terms in termsChanges">
+          <dt>{{ terms | date }}</dt>
+          <dd>{{ $t(`updates.${terms}`) }}</dd>
+        </template>
+      </dl>
+
+      <i18n path="read_all_conditions" tag="p">
+        <router-link to="/conditions">{{ $t("terms") }}</router-link>
+      </i18n>
+
+      <div class="d-flex justify-content-between">
+        <b-button variant="success" size="sm">{{ $t("approve_changes_button") }}</b-button>
+        <a v-b-toggle.unregister class="text-muted align-self-end" href="#">{{
+          $t("unregister_link")
+        }}</a>
+      </div>
+      <b-collapse id="unregister">
+        <i18n path="unregister_instructions" tag="div" class="mt-2">
+          <a href="mailto:info@locomotion.app">info@locomotion.app</a>
+        </i18n>
+      </b-collapse>
+    </b-toast>
   </div>
 </template>
 
 <script>
 import Notification from "@/mixins/Notification";
+import terms from "@/locales/views/Terms";
+import { date } from "./helpers/filters";
 
 export default {
   name: "ColocAuto",
+  methods: { date },
   mixins: [Notification],
   mounted() {
     if (this.$store.state.token && !this.$store.state.loading && !this.$store.state.loaded) {
@@ -24,8 +62,42 @@ export default {
       if( subdomain == 'test' || subdomain == 'dev' ) return subdomain;
       else if ( subdomain.match(/localhost/) ) return 'dev';
       return '';
-    }
-  }
+    },
+    approvalDate() {
+      return this.$dayjs(this.$store.state.user.terms_approved_at);
+    },
+    showTermsChanged() {
+      // Avoid showing the
+      if (this.$route.name === "conditions") {
+        return false;
+      }
+
+      if (!this.$store.state.loaded || !this.$store.state.user || !this.mostRecentUpdate) {
+        return false;
+      }
+
+      return this.approvalDate.isBefore(this.mostRecentUpdate);
+    },
+    mostRecentUpdate() {
+      return Object.keys(terms.fr.updates).sort().at(-1);
+    },
+    termsChanges() {
+      return Object.keys(terms.fr.updates)
+        .sort()
+        .filter((d) => this.approvalDate.isBefore(d));
+    },
+  },
+
+  i18n: {
+    messages: {
+      fr: {
+        ...terms.fr,
+      },
+      en: {
+        ...terms.en,
+      },
+    },
+  },
 };
 </script>
 
@@ -38,5 +110,9 @@ aside.test {
 }
 aside a {
   color: #212529;
+}
+.terms-toast {
+  max-height: calc(100vh - 16px);
+  overflow: auto !important;
 }
 </style>
---
+++ resources/app/src/locales/views/Terms.js
@@ -0,0 +1,16 @@
+export default {
+  fr: {
+    terms_changed_title: "Mise-à-jour des conditions d'utilisation",
+    terms_changed_text:
+      "Pour améliorer votre expérience, voici ce qu'on a modifié aux conditions d'utilisation:",
+    read_all_conditions: "Veuillez consulter les {0} et ré-approuver.",
+    terms: " conditions d'utilisation",
+    approve_changes_button: "C'est beau!",
+    unregister_link: "Je veux me désinscrire",
+    unregister_instructions: "Envoyez un courriel à {0} pour vous désinscrire.",
+    updates: {
+      "2022-10-06":
+        "Changement du calcul de la franchise de l'assurance de Desjardins Assurances en cas de dommages à une auto.",
+    },
+  },
+};
