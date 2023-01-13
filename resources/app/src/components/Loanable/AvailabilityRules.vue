0367899abf5acd7365e6b53de1d77c2c25483468
David Gendron <dgendron@solon-collectif.org>
2023-01-13 14:37:39 -0500

    1076 Add calendar legend.

--- resources/app/src/components/Loanable/AvailabilityRules.vue
+++ resources/app/src/components/Loanable/AvailabilityRules.vue
@@ -103,6 +103,12 @@
       </b-col>
     </b-row>
 
+    <b-row>
+      <b-col>
+        <loanable-calendar-legend />
+      </b-col>
+    </b-row>
+
     <b-row class="availability-rules__description">
       <b-col>
         <div class="form-inline availability-rules__description__default">
@@ -147,12 +153,14 @@ import VueCal from "vue-cal";
 import "vue-cal/dist/i18n/fr";
 
 import LoanableCalendar from "@/components/Loanable/Calendar.vue";
+import LoanableCalendarLegend from "@/components/Loanable/CalendarLegend.vue";
 import LoanableExceptions from "@/components/Loanable/Exceptions.vue";
 
 export default {
   name: "LoanableAvailabilityRules",
   components: {
     LoanableCalendar,
+    LoanableCalendarLegend,
     LoanableExceptions,
     VueCal,
   },
---
+++ resources/app/src/components/Loanable/CalendarLegend.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul class="loanable-calendar-legend">
+    <li class="loanable-calendar-legend__item">
+      <loanable-calendar-month-cell-content
+        class="loanable-calendar-legend__icon"
+        availability="available"
+        current
+        >XX</loanable-calendar-month-cell-content
+      >
+      Disponible
+    </li>
+    <li class="loanable-calendar-legend__item">
+      <loanable-calendar-month-cell-content
+        class="loanable-calendar-legend__icon"
+        availability="partially-available"
+        current
+        >XX</loanable-calendar-month-cell-content
+      >
+      Disponibilité limitée
+    </li>
+    <li class="loanable-calendar-legend__item">
+      <loanable-calendar-month-cell-content
+        class="loanable-calendar-legend__icon"
+        availability="unavailable"
+        current
+        >XX</loanable-calendar-month-cell-content
+      >
+      Indisponible
+    </li>
+  </ul>
+</template>
+
+<script>
+import LoanableCalendarMonthCellContent from "@/components/Loanable/CalendarMonthCellContent.vue";
+
+export default {
+  name: "LoanableCalendarLegend",
+  components: {
+    LoanableCalendarMonthCellContent,
+  },
+};
+</script>
+
+<style lang="scss">
+.loanable-calendar-legend {
+  padding: 0;
+  list-style: none;
+  display: flex;
+  justify-content: flex-start;
+
+  .loanable-calendar-legend__item {
+    display: flex;
+    align-items: center;
+  }
+
+  .loanable-calendar-legend__icon {
+    width: 3rem;
+    height: 3rem;
+  }
+}
+</style>
