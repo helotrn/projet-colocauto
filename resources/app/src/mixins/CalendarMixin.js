c26ce8f1e1f12126bdde57fb6cb2b6ee77016bd5
Fabien Quatravaux <fabien.quatravaux@riseup.net>
2023-02-24 11:50:47 +0100

    [fix] Get rid of an extra / in the loan id

--- resources/app/src/mixins/CalendarMixin.js
+++ resources/app/src/mixins/CalendarMixin.js
@@ -22,7 +22,7 @@ export default {
     },
     updateLoanDates(loan, newDates){
       return this.$store.dispatch("loans/update", {
-        id: loan.uri.replace('/loans', ''),
+        id: loan.uri.replace('/loans/', ''),
         data:{
           departure_at: this.$dayjs(newDates.start).format("YYYY-MM-DD HH:mm:ss"),
           duration_in_minutes: this.$dayjs(newDates.end).diff(newDates.start, 'minutes'),
