5458df0a07d4f7b1a4f985ec8cfae901b2a40ca7
Fabien Quatravaux <fabien.quatravaux@riseup.net>
2023-03-31 21:03:00 +0200

    [cd/ci] Improve database creation process on kubernetes

--- psql/init/initdb-postgis.sh
+++ psql/init/initdb-postgis.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-psql -U locomotion -d locomotion -c "CREATE EXTENSION IF NOT EXISTS postgis;"
-psql -U locomotion -d locomotion -c "CREATE EXTENSION IF NOT EXISTS unaccent;"
-psql -U locomotion -d locomotion -c "CREATE EXTENSION IF NOT EXISTS citext;"
-
-psql -U locomotion -c 'create database locomotion_test;'
-
-psql -U locomotion -d locomotion_test -c "CREATE EXTENSION IF NOT EXISTS postgis;"
-psql -U locomotion -d locomotion_test -c "CREATE EXTENSION IF NOT EXISTS unaccent;"
-psql -U locomotion -d locomotion_test -c "CREATE EXTENSION IF NOT EXISTS citext;"
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE EXTENSION IF NOT EXISTS unaccent;"
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE EXTENSION IF NOT EXISTS citext;"
