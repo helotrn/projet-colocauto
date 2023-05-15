e9992eb2bd5097264e76b5452394c2ce68b4b249
Clara Beaumont <clara.beaumont@mobicoop.org>
2023-05-15 07:55:14 +0000

    chore: update notification text for loan completion

--- resources/views/emails/loan/completed_text.blade.php
+++ resources/views/emails/loan/completed_text.blade.php
@@ -3,8 +3,7 @@
 @section('content')
 Bonjour {{ $user->name }},
 
-@if ($isOwner == 1)
-L'emprunt de {{ $loan->loanable->name }} de {{ $duration }} par {{ $loan->borrower->user->name }} le {{ \Carbon\Carbon::parse($loan->departure_at)->isoFormat('LL') }} s'est conclu avec succès! 
+
 @else
 Votre emprunt de {{ $loan->loanable->name }} de {{ $duration }} le {{ \Carbon\Carbon::parse($loan->departure_at)->isoFormat('LL') }} s'est conclu avec succès! 
 @endif
