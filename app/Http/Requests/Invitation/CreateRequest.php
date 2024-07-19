e4b360badf401723076c39c066bf1d320ef548eb
Fabien Quatravaux <fabien.quatravaux@riseup.net>
2024-07-19 18:18:12 +0200

    [fix] Make invitation creation possible for community admins

--- app/Http/Requests/Invitation/CreateRequest.php
+++ app/Http/Requests/Invitation/CreateRequest.php
@@ -9,23 +9,34 @@ class CreateRequest extends BaseRequest
 {
     public function authorize()
     {
-        return $this->user()->isAdmin() ||
-            ($this->user()->isCommunityAdmin() && Community::accessibleBy($this->user())->find($this->route('community_id')));
+        return $this->user()->isAdmin() || $this->user()->isCommunityAdmin();
     }
 
     public function rules()
     {
-        $rules = [
-            "email" => "string",
-        ];
 
-        return $rules;
+        $accessibleCommunityIds = implode(
+            ",",
+            Community::accessibleBy($this->user())
+                ->pluck("id")
+                ->toArray()
+        );
+
+        return [
+            "email" => "string",
+            "community_id" => [
+                "numeric",
+                "filled",
+                "in:$accessibleCommunityIds",
+            ],
+        ];
     }
 
     public function messages()
     {
         return [
             "email.required" => "L'adresse email est requise.",
+            "community_id.in" => "Vous n'avez pas accès à cette communauté.",
         ];
     }
 }
