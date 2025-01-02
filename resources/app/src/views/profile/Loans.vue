ab00ebe861b2db7ca0535de310974194ab3b347a
Fabien Quatravaux <fabien.quatravaux@riseup.net>
2025-01-02 11:22:56 +0100

    [fix] Add pagination for loans list

--- resources/app/src/views/profile/Loans.vue
+++ resources/app/src/views/profile/Loans.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="profile-loans" v-if="loaded">
-    <div class="profile-loans__loans" v-if="data.length > 0">
-      <div class="profile-loans__loans__loan" v-for="loan in data" :key="loan.id">
+    <div class="profile-loans__loans" v-if="currentCommunityLoans.length > 0">
+      <div class="profile-loans__loans__loan" v-for="loan in currentCommunityLoans" :key="loan.id">
         <loan-info-box :loan="loan" :buttons="['view']" :user="user" with-steps />
+      </div>
+      <div v-if="lastPage > 1">
+          <b-pagination
+            align="right"
+            v-model="contextParams.page"
+            :total-rows="total"
+            :per-page="contextParams.per_page"
+          />
       </div>
     </div>
     <div v-else>
@@ -24,6 +32,28 @@ export default {
   name: "ProfileLoans",
   mixins: [Authenticated, DataRouteGuards, ListMixin, UserMixin],
   components: { LoanInfoBox },
+  mounted(){
+    this.contextParams.page = 1;
+    this.contextParams['community.id'] = this.currentCommunity;
+  },
+  computed: {
+    currentCommunity() {
+      return this.$store.state.communities.current
+        ? this.$store.state.communities.current
+        : this.user.main_community.id;
+    },
+    currentCommunityLoans() {
+      return this.data.filter(e => e.community.id == this.currentCommunity)
+    }
+  },
+  watch:{
+    currentCommunity(){
+      // reload data when community change
+      this.contextParams.page = 1;
+      this.contextParams['community.id'] = this.currentCommunity
+      this.loadListData()
+    }
+  },
 };
 </script>
 
